862d6a6b7c701ce8f2647dc39f338cf6fbeb385e
DRIN DRIN <eldrinbernardino01@gmail.com>
2016-09-28 02:07:58 +0800

    Update BubbleSort.java

--- BSIS/Bernardino-Eldrin/bubble-sort/BubbleSort.java
+++ BSIS/Bernardino-Eldrin/bubble-sort/BubbleSort.java
@@ -2,52 +2,66 @@
 * BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
 * adjacent elements, then switching positions if they exist in the wrong order.
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Eldrin M. Bernardino]
+* @link    [https://github.com/ldrin01]
 * @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @since   [last updated 28/09/2016]
 */
 
 // TODO: import Scanner and Random from Java library
 
+import java.util.Scanner;
+import java.util.Random;
 public class BubbleSort {
-
-	public static void main(String[] args) {
-
-		int array[], size, x, y, swap;
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
-		array = new int[size];
-
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
-		}
-
-		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
-				}
-			}
-		}
-
-		System.out.println();
-
-		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
-	}
+    
+    public int[] arr;
+    
+    public void BubbleSort(int size){
+        arr = new int [size];
+        Random myran = new Random();
+        for(int i = 0; i < size; i++){
+            arr[i] = myran.nextInt(10000);
+        }
+        System.out.print("\t\t\t      RANDOMIZED ARRAY\n");
+        int line = 0;
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + "\t");
+            line+=1;
+            if (line == 10){
+                System.out.println();
+                line = 0;
+            }
+        }
+        int temp = 0;
+        for(int q = 0; q < size-1; q++){
+            for(int w = 0; w < (size-q)-1; w++){
+                if(arr[w] > arr[w+1]){
+                    temp = arr[w];
+                    arr[w] = arr[w+1];
+                    arr[w+1] = temp;
+                }
+            }
+        }
+        System.out.print("\n\n\t\t\t\tSORTED ARRAY\n");
+        line = 0;
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + "\t");
+            line+=1;
+            if (line == 10){
+                System.out.println();
+                line = 0;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        BubbleSort call = new BubbleSort();
+        int size = 0;
+        System.out.print("Enter array size: ");
+        size = input.nextInt();
+        call.BubbleSort(size);
+        System.out.println();
+    }
 
 }
