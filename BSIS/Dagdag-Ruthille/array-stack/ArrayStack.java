657d4fa52751b8c8eb9322f6934d3b0701b6d9d8
Ruthille Dagdag <rutheltan18@gmail.com>
2016-09-28 18:27:02 +0800

    Update ArrayStack.java

--- BSIS/Dagdag-Ruthille/array-stack/ArrayStack.java
+++ BSIS/Dagdag-Ruthille/array-stack/ArrayStack.java
@@ -2,84 +2,72 @@
 * Java implementation of a stack using array.
 * For demo purpose, should hold a maximum of 10 string values.
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Ruthille Dagdag]
+* @link    [https://github.com/lvcc-dsa/Students/edit/master/BSIS/Dagdag-Ruthille/array-stack/ArrayStack.java]
 * @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @since   [28 september 2016]
 */
 
 public class ArrayStack {
 
-  // note: variable declarations
-  private int capacity; // note: max length of array
-  private int top = 0; // note: initialize a default value of top
-  private String[] storage; // note: array is of string values
+ 
+  private int capacity; 
+  private int top = 0; 
+  private String[] storage; 
   
-  // note: constructor = initialize an object instance of the class
   public ArrayStack(int size) {
-    // todo: pass the value of size to capacity
-    storage = new String[/*__________*/]; // todo: update the array capacity
+    
+    storage = new String[size]; 
   }
 
-  // note: will display the array
   public void show() {
-    // note: the top of the stack starts from array[0] then newer item gets added on top of another until it reaches its capacity limit
+    
     for (int i = capacity-1; i >= 0; i--) {
-      /* note: should print like this: 
-      stack[9]   = 10th value
-      stack[8]   = 9th value
-      stack[7]   = 8rd value
-      ....  
-      stack[1]   = 2nd value
-      stack[0]   = 1st value
-      */
+      System.out.println("stack[" + i + "] = " + storage[i] );
     }
-    System.out.println(); // note: prints optional new line for readability
+    System.out.println(); 
   }
   
-  // note: will check if it's true that the array is empty
   private boolean isEmpty() {
-    if (storage[0] == null) { // note: compares if index[0] or top is null or empty
-      System.out.println(" "); // todo: print error message here
-      System.out.println(); // note: this prints optional new line for readability
+    if (storage[0] == null) { 
+      System.out.println("Storage is empty "); 
+      System.out.println();
       return true;
     } 
-    return false; // note: defaults to false
+    return false; 
   }
 
-  // note: will check if it's true that the array is full
+  
   private boolean isFull() {
-    if (top == capacity) { // note: compares if top has reached capacity limit
-      System.out.println(" "); // todo: print error message here
+    if (top == capacity) { 
+      System.out.println("Storage is full");
       return true;
     } 
-    return false; // note: defaults to false
+    return false; 
   }
 
-  // note: will add value to the array
   public void push(String value) {
     System.out.println("... trying to push on stack[" + top + "] ...");
     if (isFull()) {
-      System.out.println(" "); // todo: print error message here
-      System.out.println(); // note: prints optional new line for readability
+      System.out.println("ADD FAILED "); 
+      System.out.println(); 
     } else {      
-      storage[top]; // todo: assign the value to top
-      top; // todo: should increment or decrement?
-      System.out.println(value + " was successfully _________."); // todo: what does push do?
-      System.out.println(); // prints optional new line for readability
-    }
+      storage[top]; 
+      top++; 
+      System.out.println(value + " was successfully added."); 
+      System.out.println(); }
   }
+ 
 
-  // note: will remove value to the array
   public void pop() {
     System.out.println("... trying to pop stack[" + (top-1) + "] ...");
-    if (isEmpty()) { // note: checks if empty, then stop
-      System.out.println(" "); // print error message here
-      System.out.println(); // prints optional new line for readability
+    if (isEmpty()) { 
+      System.out.println("POP FAILED"); 
+      System.out.println(); 
     } else {      
-      storage[top]; // todo: empty the value of top
-      top; // todo: should increment or decrement?
-      System.out.println(storage[top] + " was successfully _________."); // todo: what does pop do?
+      storage[top]; 
+      top;
+      System.out.println(storage[top] + " was successfully removed."); // todo: what does pop do?
       System.out.println();
     }
   }
