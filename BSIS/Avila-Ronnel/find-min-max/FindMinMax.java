00f1a8aaeb2a6e3fba65c84b5cf70212c46f1844
Ronnel Dime Avila <nelronelavila1999@gmail.com>
2016-09-28 18:18:16 +0800

    Update FindMinMax.java

--- BSIS/Avila-Ronnel/find-min-max/FindMinMax.java
+++ BSIS/Avila-Ronnel/find-min-max/FindMinMax.java
@@ -1,3 +1,11 @@
+/*
+@author  Ronnel Avila
+* @link    https://github.com/nelronel-avila19
+* @version 1.0
+* @since   28/09/2016
+*/
+
+
 public class FindMinMax {
 	public static void main(String[] args) {
 		int[] numbers = { 88, 33, 55, 23, 64, 123 };
