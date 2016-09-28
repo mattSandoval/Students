383930f9ed69c68af6b2cf6aa8343af802b87168
javened <jave21ned@gmail.com>
2016-09-28 16:58:05 +0800

    Update BubbleSort.java

--- BSIS/Nedia-Javee/bubble-sort/BubbleSort.java
+++ BSIS/Nedia-Javee/bubble-sort/BubbleSort.java
@@ -2,52 +2,50 @@
 * BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
 * adjacent elements, then switching positions if they exist in the wrong order.
 *
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @author  [Javee Nedia]
+* @link    [https://github.com/lvcc-dsa/Students/blob/master/BSIS/Nedia-Javee/bubble-sort/BubbleSort.java]
+* @version [v 1]
+* @since   [28/9/2016]
 */
 
 // TODO: import Scanner and Random from Java library
 
-public class BubbleSort {
-
-	public static void main(String[] args) {
-
-		int array[], size, x, y, swap;
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
-		array = new int[size];
-
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
-		}
-
-		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
-				}
-			}
-		}
-
-		System.out.println();
-
-		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
-	}
-
+import java.util.Scanner;
+import java.util.Random;
+public class BubbleSort{
+  public static void main(String[]args){
+  int size, x, y, swap, array[];
+  Scanner input = new Scanner(System.in);
+  Random rand = new Random();
+  System.out.print("Enter Array size to Sort: ");
+  size = input.nextInt();
+  array = new int [size];
+  System.out.println("\n" + size + " Random numbers to be generated");
+  System.out.println();
+  for(x = 0; x < size; x++){
+    if(x%10 == 0){
+      System.out.println();
+    }
+    array[x] = rand.nextInt(10000);
+    System.out.print(array[x] + "\t");
+  }
+  System.out.println("\n\n" + size + " Random numbers are generated");
+  System.out.println();
+  for(x = 0;x < size; x++){
+    for(y = 0; y < x; y++){
+      if(array[y] > array[y+1]){
+      swap = array[y+1];
+      array[y+1] = array[y];
+      array[y] = swap;
+      }
+    }
+  }
+  System.out.println("Bubble Sorting.....DONE!");
+  for(x = 0; x < size; x++){
+    if(x%10 == 0){
+      System.out.println();
+    }
+    System.out.print(array[x] + "\t");
+  }
+  }
 }
