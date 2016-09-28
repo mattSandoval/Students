5bd3dee2a5c0eb8443a1d0d009976e0749c14e2f
Kizaken <lie.ruina@gmail.com>
2016-09-28 18:15:58 +0800

    Update LinearSearch.java

--- BSIS/Quiza-Charlie/linear-search/LinearSearch.java
+++ BSIS/Quiza-Charlie/linear-search/LinearSearch.java
@@ -6,38 +6,77 @@
 * @version 1.1
 * @since   22/09/2016
 */
-
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class LinearSearch {
+	
 
-	public static void main(String[] args) {
+	int [] array;
+	double average = 0;
+	int arraySize;
+	private int toSearch;
+	
+	public LinearSearch (int size){
+		arraySize = size;
+		array = new int [arraySize];
 
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
+		
 	}
-
+	public void getArray(){
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < arraySize; i++){
+			array[i]=rand.nextInt(100);
+			System.out.println("Random[" +i+ "] = " + array[i]);
+		}
+		System.out.println();
+	}
+	
+	
+	public void show(){
+		
+		for (int i = 0; i < arraySize; i++){
+			
+			System.out.println ( array[i]);
+		}
+		
+	}
+	
+	public void search(){
+		Scanner input = new Scanner(System.in);
+		int i;
+		System.out.print("Plese enter the number to be search : ");
+		toSearch = input.nextInt();
+		
+		for(i = 0; i<array.length;i++){
+			if(toSearch == array[i]){
+				System.out.print("The value has been found in array["+i+"] = "+array[i]);
+				break;
+			}
+			
+		}
+		if(i == array.length){
+			System.out.print("The value not found!");		
+			}
+	}
+	
+	public static void main (String[] Args){
+		
+		int size;
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter array size: " );
+		size = input.nextInt();
+		
+		
+		LinearSearch summon = new LinearSearch (size);
+		summon.getArray();
+		summon.search();
+		
+		
+		
+	
+	}
 }
