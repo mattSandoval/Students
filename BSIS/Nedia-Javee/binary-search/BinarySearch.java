3a2868aba63c594241cce2fbc65a3c32510fb580
javened <jave21ned@gmail.com>
2016-09-28 16:51:35 +0800

    Update BinarySearch.java

--- BSIS/Nedia-Javee/binary-search/BinarySearch.java
+++ BSIS/Nedia-Javee/binary-search/BinarySearch.java
@@ -8,66 +8,69 @@
 * @since   [last updated dd/mm/yyyy]
 */
 
-import java.util.*; // note: imports all classes inside java.util
-
-public class BinarySearch {
-
-	static int array[], index, size, search;
-
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-
-		System.out.println(size + " random numbers generated.");
-		
-		for (int i = 0; i < array.length; i++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-		}
-		
-		// TODO: call bubbleSort method to sort the generated random numbers
-		System.out.println();
-		
-		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
-		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
-		
-		if (index != -1) {
-			System.out.println("FOUND: " + search + " is at " + "array["+index+"]");
-		}
-		else {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
-	static int binarySearch(int[] numbers, int find) { // note: @param array[], search
-
-		int left, right, middle;
-		// TODO: initialize value for left and right
-		
-		while (left <= right) {
-			middle = left + (right - left) / 2;
-			if (numbers[middle] == find) {
-				return middle;
-			} else if (find < numbers[middle]) {
-				right = middle - 1;
-			} else { // if numbers[middle] < find
-				left = middle + 1;
-			}
-		}
-		return -1; // note: exit loop if not found
-
-	}
-	
-	static int bubbleSort(int[] arr) {
-
-		// TODO: sort the random array first before searching
-
-	}
-
+import java.util.Scanner;
+import java.util.Random;
+public class BinarySearch{
+  private int capacity, array[];
+  
+  public void sorting(){
+  int x, y, temp;
+  for(x = 0; x < capacity-1; x++){
+    for(y = 0; y < capacity-1; y++){
+      if (array[y] > array[y+1]){
+        temp = array[y+1];
+        array[y+1] = array[y];
+        array[y] = temp;
+      }
+    }
+  }
+  for(int a =0; a < capacity;a++){
+    if(x%10 == 0){
+      System.out.println();
+    }
+    System.out.print(array[a]+ "\t");
+  }
+  }
+ 
+  
+  
+  public BinarySearch(int num){
+  capacity = num;
+  array = new int [capacity];
+  }
+  
+  public void searching(int num){
+    int left = 0, middle, right = array.length-1;
+    while(left <= right){
+      middle=(left+right)/2;
+      if(array[middle] == num){
+        System.out.println("FOUND: " + num + " is at array[" + middle + "].");
+        break;
+      }
+      else if(array[middle] < num)
+        left = middle+1;
+      else if(array[middle] > num)
+        right = middle-1;
+    }
+    if(left >= right){
+      System.out.println("NOT FOUND: " + num + " is not in the array.");
+    }
+  }
+  public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+    int size, number;
+   System.out.print("Please input array size: ");
+   size = input.nextInt();
+   BinarySearch BinarySearch = new BinarySearch(size);
+   for(int i = 0; i < BinarySearch.capacity; i++){
+      BinarySearch.array[i] = (int) (1+Math.random()*1000);
+    }
+   Scanner in = new Scanner(System.in); 
+   BinarySearch.sorting();
+   System.out.println();
+    System.out.print("Please enter the number to find: ");
+    number = in.nextInt();
+   System.out.println();
+   BinarySearch.searching(number);
+  }
 }
