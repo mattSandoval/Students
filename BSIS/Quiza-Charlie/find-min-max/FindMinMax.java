de8f89416683472042a0e8c74b1c06614b8a0411
Kizaken <lie.ruina@gmail.com>
2016-09-28 18:02:42 +0800

    Update FindMinMax.java

--- BSIS/Quiza-Charlie/find-min-max/FindMinMax.java
+++ BSIS/Quiza-Charlie/find-min-max/FindMinMax.java
@@ -1,15 +1,23 @@
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
+
+	public static void main (String[] args){
+		
+		int[] num = {88, 33, 55, 23 , 64, 123 };
 		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		int max = Integer.MIN_VALUE; 
+		
+		for (int i= 0; i < num.length; i++) { 
+			
+			if (num [i] < min )
+				min = num [i];
+			if (num[i] > max)
+				
+				max = num [i]; 
+				
+						
+			
 	}
+	System.out.println("Smallest number is " + min+".");
+	System.out.println("Largest number is " + max+".");
+}
 }
