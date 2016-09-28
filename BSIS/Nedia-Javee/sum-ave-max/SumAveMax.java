c71d394cfa62ca9939e8b10a2bd6750bd3142ce0
javened <jave21ned@gmail.com>
2016-09-28 17:53:20 +0800

    Update SumAveMax.java

--- BSIS/Nedia-Javee/sum-ave-max/SumAveMax.java
+++ BSIS/Nedia-Javee/sum-ave-max/SumAveMax.java
@@ -9,52 +9,178 @@
 * @since   [last updated dd/mm/yyyy]
 */
 
+
 import java.util.Scanner;
 import java.util.Random;
 
-public class SumAveMax {
-
-	private int sum, max, size, array[];
-	private float average;
-
-	public SumAveMax() {
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-		for (int i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			System.out.print(array[i] + " ");
-			sum += array[i];
-			if (array[i] > max)
-				max = array[i];			
+public class totalMax {
+	private int []array;
+	private int capacity;
+	private int sum=0;
+	private double average=0;
+	private int max=0;
+	
+	private totalMax(int num){
+		capacity=num;
+		array=new int[capacity];
+	}
+	public void input(){
+		for(int i=0;i<capacity;i++){
+			array[i]=(int)(1+Math.random()*1000);
+		}
+		for(int i=0;i<capacity;i++){
+			if (i%10==0){
+				System.out.println();
+			}
+			System.out.print(array[i] + "\t");
 		}
 		System.out.println();
 	}
-
-	public void getSum() {
-		System.out.println("SUM = " + sum);
+	public void sum(){
+		for(int i = 0;i<capacity;i++){
+			sum+=array[i];
+		}
+		System.out.println("Sum = "+sum);
+	}
+	public void average(){
+		average=sum/capacity;
+		System.out.println("Average = "+average);
+	}
+	public void max(){
+		for(int i = 0;i<capacity;i++){
+			if(array[i]>max){
+				max=array[i];
+			}
+		}
+		System.out.println("Max = "+max);
 	}
 
-	public void getMax() {
-		System.out.println("MAX = " + max);
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		int amount;
+		System.out.print("Please input amount : ");
+		amount=input.nextInt();
+		totalMax totalMax=new totalMax(amount);
+		totalMax.input();
+		totalMax.sum();
+		totalMax.average();
+		totalMax.max();
 	}
+	
+}
+import java.util.Scanner;
+import java.util.Random;
 
-	public void getAverage() {
-		// note: type casting = assigning a value of one type to a variable of another type
-		average = (float) sum / size; // note: type cast int to float
-		System.out.println("AVERAGE = " + average);
+public class totalMax {
+	private int []array;
+	private int capacity;
+	private int sum=0;
+	private double average=0;
+	private int max=0;
+	
+	private totalMax(int num){
+		capacity=num;
+		array=new int[capacity];
 	}
-
-	public static void main(String[] args) {
-		SumAveMax demo = new SumAveMax();
+	public void input(){
+		for(int i=0;i<capacity;i++){
+			array[i]=(int)(1+Math.random()*1000);
+		}
+		for(int i=0;i<capacity;i++){
+			if (i%10==0){
+				System.out.println();
+			}
+			System.out.print(array[i] + "\t");
+		}
 		System.out.println();
-		demo.getSum();
-		demo.getMax();
-		demo.getAverage();
+	}
+	public void sum(){
+		for(int i = 0;i<capacity;i++){
+			sum+=array[i];
+		}
+		System.out.println("Sum = "+sum);
+	}
+	public void average(){
+		average=sum/capacity;
+		System.out.println("Average = "+average);
+	}
+	public void max(){
+		for(int i = 0;i<capacity;i++){
+			if(array[i]>max){
+				max=array[i];
+			}
+		}
+		System.out.println("Max = "+max);
 	}
 
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		int amount;
+		System.out.print("Please input amount : ");
+		amount=input.nextInt();
+		totalMax totalMax=new totalMax(amount);
+		totalMax.input();
+		totalMax.sum();
+		totalMax.average();
+		totalMax.max();
+	}
+	
+}
+import java.util.Scanner;
+import java.util.Random;
+
+public class totalMax {
+	private int []array;
+	private int capacity;
+	private int sum=0;
+	private double average=0;
+	private int max=0;
+	
+	private totalMax(int num){
+		capacity=num;
+		array=new int[capacity];
+	}
+	public void input(){
+		for(int i=0;i<capacity;i++){
+			array[i]=(int)(1+Math.random()*1000);
+		}
+		for(int i=0;i<capacity;i++){
+			if (i%10==0){
+				System.out.println();
+			}
+			System.out.print(array[i] + "\t");
+		}
+		System.out.println();
+	}
+	public void sum(){
+		for(int i = 0;i<capacity;i++){
+			sum+=array[i];
+		}
+		System.out.println("Sum = "+sum);
+	}
+	public void average(){
+		average=sum/capacity;
+		System.out.println("Average = "+average);
+	}
+	public void max(){
+		for(int i = 0;i<capacity;i++){
+			if(array[i]>max){
+				max=array[i];
+			}
+		}
+		System.out.println("Max = "+max);
+	}
+
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		int amount;
+		System.out.print("Please input amount : ");
+		amount=input.nextInt();
+		totalMax totalMax=new totalMax(amount);
+		totalMax.input();
+		totalMax.sum();
+		totalMax.average();
+		totalMax.max();
+	}
+	
 }
