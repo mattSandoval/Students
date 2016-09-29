205225c45446ffe50077cdb160cc36c031906845
Michael John Isip <mj.isip123@gmail.com>
2016-09-30 00:37:06 +0900

    Update ArrayStack.java

--- BSIS/Isip-Michael-John/array-stack/ArrayStack.java
+++ BSIS/Isip-Michael-John/array-stack/ArrayStack.java
@@ -1,159 +1,122 @@
 /**
-* Java implementation of a stack using array.
-* For demo purpose, should hold a maximum of 10 string values.
-*
+* A STACK is an ADT (abstract data type) in LIFO order (last in, first out).
+* 
 * @author  [Michael John Isip]
 * @link    [https://github.com/mj-isip23]
-* @version [version number]
-* @since   [09-28-2016]
+* @version [1.1]
+* @since   [9-26-2016]
 */
 
-
 public class ArrayStack {
-
-  // note: variable declarations
-  private int capacity; // note: max length of array
-  private int top = 0; // note: initialize a default value of top
-  private String[] storage; // note: array is of string values
-  
-  // note: constructor = initialize an object instance of the class
+    
+  private String[] arrayStack;
+  private int arraySize;
+  private int top=0;
+    
+    
   public ArrayStack(int size) {
-    // todo: pass the value of size to capacity
-      capacity = size;
-    storage = new String[capacity]; // todo: update the array capacity
+      arraySize = size;
+      arrayStack = new String[arraySize];
   }
-
-  // note: will display the array
-  public void show() {
-    // note: the top of the stack starts from array[0] then newer item gets added on top of another until it reaches its capacity limit
-    for (int i = capacity-1; i >= 0; i--) {
-      /* note: should print like this: 
-      stack[9]   = 10th value
-      stack[8]   = 9th value
-      stack[7]   = 8rd value
-      ....  
-      stack[1]   = 2nd value
-      stack[0]   = 1st value
-      */
-        System.out.println("stack[" + i + "]   =   " + storage[i]);
-    }
-    System.out.println(); // note: prints optional new line for readability
-  }
-  
-  // note: will check if it's true that the array is empty
-  private boolean isEmpty() {
-    if (storage[0] == null) { // note: compares if index[0] or top is null or empty
-      System.out.println("Stack is EMPTY!"); // todo: print error message here
-      System.out.println(); // note: this prints optional new line for readability
-      return true;
-    } 
-    return false; // note: defaults to false
-  }
-
-  // note: will check if it's true that the array is full
-  private boolean isFull() {
-    if (top == capacity) { // note: compares if top has reached capacity limit
-      System.out.println("Stack is FULL!"); // todo: print error message here
-      return true;
-    } 
-    return false; // note: defaults to false
-  }
-
-  // note: will add value to the array
-  public void push(String value) {
-    System.out.println("... trying to push on stack[" + top + "] ...");
-    if (isFull()) {
-      System.out.println("Cannot push a value"); // todo: print error message here
-      System.out.println(); // note: prints optional new line for readability
-    } else {      
-      storage[top] = value; // todo: assign the value to top
-      top++; // todo: should increment or decrement?
-      System.out.println(value + " was successfully added."); // todo: what does push do?
-      System.out.println(); // prints optional new line for readability
-    }
-  }
-
-  // note: will remove value to the array
-  public void pop() {
-    System.out.println("... trying to pop stack[" + (top-1) + "] ...");
-    if (isEmpty()) { // note: checks if empty, then stop
-      System.out.println("Cannot pop a value"); // print error message here
-      System.out.println(); // prints optional new line for readability
-    } else {      
-      storage[top] = null; // todo: empty the value of top
-      top--; // todo: should increment or decrement?
-      System.out.println(storage[top] + " was successfully removed."); // todo: what does pop do?
-      System.out.println();
-    }
-  }
-
-  // note: will show the topmost value of the array
-  public void peek() {
-    if (isEmpty()) { 
-      System.out.println("PEEK TOP = NULL");
-      System.out.println();
-    } else {
-      System.out.println("PEEK TOP = " + storage[top]);
-      System.out.println();
-    }
-  }
-  
   public static void main(String[] args) {
-    // construct a new array queue and assign 10 as the integer value for the size limit of the stack
-      ArrayStack storage = new ArrayStack(10);
-    System.out.println("STORAGE CAPACITY = " + storage.capacity);
-    System.out.println();
-    // show empty stack
-    storage.show(); 
+      ArrayStack obj = new ArrayStack(10);
+      
+      System.out.println("STACK SIZE = " + obj.arraySize);
+      System.out.println();
 
-    // try removing on an empty stack
-    storage.pop(); 
-    // peek top element of an empty stack
-    storage.peek(); 
-
-    storage.push("one");
-    // show the updated stack
-    storage.show(); 
-    // peek if top element is "one"
-    storage.peek(); 
-
-    storage.push("two");
-    // show the updated stack
-    storage.show(); 
-    // peek if top element is "two"
-    storage.peek(); 
-
-    storage.push("three");
-    // show the updated stack
-    storage.show(); 
-    // peek if top element is "three"
-    storage.peek(); 
-
-    storage.push("four");
-    // show the updated stack
-    storage.show(); 
-
-    storage.push("five"); 
-    // show the updated stack
-    storage.show(); 
-    // try removing "five"
-    storage.pop(); 
-
-    storage.push("six"); 
-    storage.push("seven"); 
-    storage.push("eight"); 
-    storage.push("nine");    
-    storage.push("ten"); 
-    // show the updated stack
-    storage.show(); 
-    // peek if top element is "ten"
-    storage.peek(); 
-    // try adding "eleven"
-    storage.push("eleven"); 
-    // try adding "twelve"
-    storage.push("twelve"); 
-    // show the updated stack
-    storage.show(); 
-  }  
-
+      //Show while stack is Empty
+      obj.Show();
+      
+      //Push an element
+      obj.Push("mj");
+      
+      //Peek top element
+      obj.Peek();
+      
+      //Pop an element
+      obj.Pop();
+      
+      obj.Show();
+      
+      //Peek on Empty stack
+      obj.Peek();
+      
+      //Pop on Empty stack
+      obj.Pop();
+      
+      obj.Push("one");
+      obj.Push("two");
+      obj.Push("three");
+      obj.Push("four");
+      obj.Push("five");
+      obj.Push("six");
+      obj.Push("seven");
+      obj.Push("eight");
+      obj.Push("nine");
+      obj.Push("ten");
+      
+      //Push on Full stack
+      obj.Push("eleven");
+      
+      obj.Show();
+  }
+  
+  public void Show(){
+      for(int i=arraySize-1; i>=0; i--){
+          System.out.println("Stack [" + i + "] = " + arrayStack[i]);
+      }
+      System.out.println();      
+  }
+  
+  public void Peek(){
+      if(IsEmpty()){
+          System.out.println("Peek is null. Stack is EMPTY!");
+          System.out.println();
+      } else {
+          System.out.println("TOP: Stack [" + (top-1) + "] = " + arrayStack[top-1]);          
+          System.out.println();
+      }
+  }
+  
+  public void Push(String input){
+      if (IsFull()){
+          System.out.println("Cannot PUSH anymore. Stack is FULL!");
+          System.out.println();
+      } else {
+          arrayStack[top] = input;
+          top++;
+          System.out.println("Stack [" + (top-1) + "] = " + arrayStack[top-1]);
+          System.out.println("--- Successful PUSH.");          
+          System.out.println();
+      }
+  }
+  
+  public void Pop(){
+      if(IsEmpty()){
+          System.out.println("Cannot POP an element. Stack is EMPTY!");
+          System.out.println();
+      } else {
+          System.out.println("Stack [" + (top-1) + "] = " + arrayStack[top-1]);
+          arrayStack[top-1] = null;
+          top--;
+          System.out.println("--- Successful POP.");
+          System.out.println();
+      }
+  }
+  
+  public boolean IsEmpty(){
+      if (top == 0){
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  public boolean IsFull(){
+      if(top == arraySize){
+          return true;
+      } else {
+          return false;
+      }
+  }
 }
