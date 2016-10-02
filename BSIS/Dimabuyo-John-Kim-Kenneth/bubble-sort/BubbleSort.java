9d313eb3d8d8478f25ffd3ba67d21a0ef7e40ee0
Dimabuyo, John Kim Kenneth D <kennethdimabuyo@yahoo.com>
2016-10-02 17:53:54 +0800

    Add files via upload

--- BSIS/Dimabuyo-John-Kim-Kenneth/bubble-sort/BubbleSort.java
+++ BSIS/Dimabuyo-John-Kim-Kenneth/bubble-sort/BubbleSort.java
@@ -1,53 +1,40 @@
-/**
-* BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
-* adjacent elements, then switching positions if they exist in the wrong order.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
-
-// TODO: import Scanner and Random from Java library
+import java.util.*;
 
 public class BubbleSort {
-
 	public static void main(String[] args) {
-
-		int array[], size, x, y, swap;
-
+		
+		int n, c, d, swap;
 		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
 
-		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
-		array = new int[size];
+		System.out.print ("Please enter the array size: ");
+		n = input.nextInt();
 
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
+		int array[] = new int [n];
 
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
+		System.out.println ("Enter " + n + " integers");
+
+		for (int i=0; i < n ; i++ ) {
+			array[i] = input.nextInt();
 		}
-
-		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
+		
+		System.out.println("Sorting ... Please wait");
+		
+		//swaping time!
+		for (c = 0; c < n - 1; c++) {
+			for (d =0; d < n-1-c; d++) {
+				if (array[d] < array[d+1])
+				{
+					swap = array[d];
+					array[d] = array [d+1];
+					array[d+1] = swap;
 				}
 			}
 		}
-
-		System.out.println();
-
-		System.out.println("Bubble Sorting... Done!");
 		
-		// TODO: print sorted numbers separated by space
+		System.out.println("Successfully sorted!");
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println(array[i]);
+		}
 	}
-
 }
