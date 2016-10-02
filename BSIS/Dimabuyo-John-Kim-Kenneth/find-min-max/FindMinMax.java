cb873efec4209168c5e8be66f13a54fd0925cf11
Dimabuyo, John Kim Kenneth D <kennethdimabuyo@yahoo.com>
2016-10-02 18:00:35 +0800

    Add files via upload

--- BSIS/Dimabuyo-John-Kim-Kenneth/find-min-max/FindMinMax.java
+++ BSIS/Dimabuyo-John-Kim-Kenneth/find-min-max/FindMinMax.java
@@ -1,6 +1,8 @@
+
 public class FindMinMax {
+	
 	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
+		int [] numbers = { 88, 33, 55, 23, 64, 123};
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < numbers.length; i++) {
@@ -10,6 +12,7 @@ public class FindMinMax {
 				max = numbers[i];
 		}
 		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		System.out.println("Largest number is " + max);
 	}
+
 }
