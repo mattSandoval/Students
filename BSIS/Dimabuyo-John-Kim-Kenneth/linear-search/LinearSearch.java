fc2ef36fdbca64f8d4be1b430bb8c6d7f76eda2a
Dimabuyo, John Kim Kenneth D <kennethdimabuyo@yahoo.com>
2016-10-02 18:45:37 +0800

    Add files via upload

--- BSIS/Dimabuyo-John-Kim-Kenneth/linear-search/LinearSearch.java
+++ BSIS/Dimabuyo-John-Kim-Kenneth/linear-search/LinearSearch.java
@@ -1,43 +1,59 @@
-/**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
-*/
-
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 public class LinearSearch {
-
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
+	public int [] array;
+	private int search;
+	
+	public void setter(int size) {
+		array = new int [size];
+		Random rand = new Random ();
+		for (int i = 0; i < size; i++){
+			array [i] = rand.nextInt(100);
 		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
 	}
-
+	
+	public void show () {
+		System.out.println("");
+		for (int i =0; i < array.length; i++) {
+			System.out.println("Array [" + (i+1) + "] = " + array [i]);
+		}
+		System.out.println("");
+	}
+	
+	
+	public void searching(int size){
+		int i;
+		for (i =0 ; i < array.length; i++) {
+			if (array[i] == size) {
+				System.out.println("The number was found on Array[" + (i+1) + "]");
+				break;
+			}
+		}
+		if (i == array.length){
+			System.out.println("The value you input not found");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		LinearSearch call = new LinearSearch();
+		int yes;
+		
+		System.out.print("Enter the Array size: ");
+		int ArraySize = input.nextInt();
+		call.setter(ArraySize);
+		
+		System.out.print("What number do you want to search: ");
+		int search = input.nextInt();
+		call.searching(search);
+		
+		System.out.println("Do you want to show the entire Array? [1/0]");
+		yes = input.nextInt();
+		if (yes == 1){
+			call.show();
+		} else {
+			System.out.println("Thank you for using my program! :)");
+		}
+		
+	}
 }
