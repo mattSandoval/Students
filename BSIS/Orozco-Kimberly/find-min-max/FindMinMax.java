1130c5208fa84b54dbc9c614e8f870b0a2014684
kimOrozco <kimpossible1204@gmail.com>
2016-10-05 15:50:27 +0800

    Update FindMinMax.java

--- BSIS/Orozco-Kimberly/find-min-max/FindMinMax.java
+++ BSIS/Orozco-Kimberly/find-min-max/FindMinMax.java
@@ -1,3 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+* @author: Kimberly Orozco
+* @link: https://github.com/lvcc-dsa/Students/edit/master/BSIS/Orozco-Kimberly/find-min-max/FindMinMax.java
+* @version: n/a
+* @since: [10/05/16]
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
 public class FindMinMax {
 	public static void main(String[] args) {
 		int[] numbers = { 88, 33, 55, 23, 64, 123 };
