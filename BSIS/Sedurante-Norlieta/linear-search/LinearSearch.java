8ac47c5cc89b4113c82a02c1729c0056a5127e3b
norlietasedurante <norlietasedurante@gmail.com>
2016-10-05 17:23:52 +0800

    Update LinearSearch.java

--- BSIS/Sedurante-Norlieta/linear-search/LinearSearch.java
+++ BSIS/Sedurante-Norlieta/linear-search/LinearSearch.java
@@ -1,11 +1,13 @@
-/**
+/*** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 * LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
+* SOURCE CODE: LinearSearch.java
+* AUTHOR NAME: Sedurante, Norlieta I.
+* GITHUB URL: [https://github.com/lvcc-dsa/Students/BSIS/Sedurante-Norlieta/linear-search/LinearSearch.java]
+* LAST UPDATE: [10/05/2016]
 *
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 */
+
 
 import java.util.Scanner;
 import java.util.Random;
@@ -27,7 +29,7 @@ public class LinearSearch {
 		search = input.nextInt();
 
 		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
+			array[i] = rand.nextInt(2000);
 			if (array[i] == search) {
 				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
 				break;
