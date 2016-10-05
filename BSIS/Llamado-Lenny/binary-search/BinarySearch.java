72e14af19da2d13333dff7f5b74ee694f349bbf1
Lenny Llamado <seveven88@gmail.com>
2016-10-05 16:41:44 +0800

    Update BinarySearch.java

--- BSIS/Llamado-Lenny/binary-search/BinarySearch.java
+++ BSIS/Llamado-Lenny/binary-search/BinarySearch.java
@@ -1,73 +1,85 @@
-/**
-* BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
-* Each step, the size of array is reduced to half until one single element remain and a match is found.
-*
-* @author  [your full name here]
-* @link    [full github url]
+
+/* @author  [Lenny Llamado]
+* @link    [https://github.com/lvcc-dsa/Students/edit/master/BSIS/Llamado-Lenny/binary-search/BinarySearch.java]
 * @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @since   [05/10/2016]
 */
 
-import java.util.*; // note: imports all classes inside java.util
+package binarysearch;
+import java.util.*;
 
 public class BinarySearch {
+    static int index,size, search, array[];
 
-	static int array[], index, size, search;
+    public static void main(String[] args) {
+       Scanner leng = new Scanner (System.in);
+       Random LL = new Random();
+       System.out.print("Enter array size: ");
+       size = leng.nextInt();
+       array = new int [size];
+       System.out.print(size+ " random numbers generated.");
+       System.out.println();
+       for(int i=0; i < array.length; i++){
+           array[i] = LL.nextInt(1000);
+       }
 
-	public static void main(String[] args) {
+       bubbleSort (array);
+       System.out.println();
+       System.out.print("Enter number to find: ");
+       search = leng.nextInt();
+       index = BinarySearch(array, search);
 
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
+       if (index != -1) {
+          System.out.print("Found: " +search+ " is at " + "array ["+index+"]");
+       }
 
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
+       else {
+          System.out.println("Not found: " +search+ " is not in the array.");
 
-		System.out.println(size + " random numbers generated.");
-		
-		for (int i = 0; i < array.length; i++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-		}
-		
-		// TODO: call bubbleSort method to sort the generated random numbers
-		System.out.println();
-		
-		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
-		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
-		
-		if (index != -1) {
-			System.out.println("FOUND: " + search + " is at " + "array["+index+"]");
-		}
-		else {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
+       }
+   }
+    static int BinarySearch(int[] numbers, int find){
 
-	}
+      int left, right, middle;
+      left = 0;
+      right = numbers.length - 1;
 
-	static int binarySearch(int[] numbers, int find) { // note: @param array[], search
+       while (left <= right){
+        middle = left + (right - left)/2;
 
-		int left, right, middle;
-		// TODO: initialize value for left and right
-		
-		while (left <= right) {
-			middle = left + (right - left) / 2;
-			if (numbers[middle] == find) {
-				return middle;
-			} else if (find < numbers[middle]) {
-				right = middle - 1;
-			} else { // if numbers[middle] < find
-				left = middle + 1;
-			}
-		}
-		return -1; // note: exit loop if not found
+        if (numbers[middle] == find){
+          return middle;
 
-	}
-	
-	static int bubbleSort(int[] arr) {
+        }
+        else if (find < numbers[middle]){
+          right = middle - 1;
+        }
+        else {
+          left = middle +1;
+        }
+      }
+      return -1;
+    }
 
-		// TODO: sort the random array first before searching
+    static int bubbleSort(int[] arr){
+      int x, y , swap;
+      for (x=0; x < size-1 ; x++) {
+        for (y = 0; y < size-x-1; y++) {
 
-	}
+          if (array[y] > array[y+1]) {
+            swap = array[y];
+            array[y] = array[y+1];
+            array[y+1] = swap;
+            
+          }
+        }
+      }
+
+      for (x=0; x<size ; x++ ) {
+          System.out.print(array[x] + " "); 
+      }
+
+      return 0;
+    }
 
 }
