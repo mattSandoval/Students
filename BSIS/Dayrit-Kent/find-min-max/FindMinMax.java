e551392c34ed49d4911ebdeddbf8966d9edfb0de
KDpogz <bernz19XD@gmail.com>
2016-10-05 15:31:52 +0800

    Update FindMinMax.java

--- BSIS/Dayrit-Kent/find-min-max/FindMinMax.java
+++ BSIS/Dayrit-Kent/find-min-max/FindMinMax.java
@@ -1,15 +1,26 @@
+//Dayrit Kent
+//https://github.com/lvcc-dsa/Students/edit/master/BSIS/Dayrit-Kent/find-min-max/FindMinMax.java
+//ver 1
+//10/6/16
+package jrandom;
+
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
+
+	public static void main(String[]args){
+		int[] number = {88,33,55,23,64,23};
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
+		
+		for (int i = 0; i<number.length;i++){
+			if(number[i]<min){
+				min = number[i];
+			}
+		if(number[i]>max){
+			max = number[i];
 		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		}
+		
+		System.out.println("Smallest Number is "+ min);
+		System.out.println("The Highest Number is "+ max);
 	}
 }
