182366646c3fe8c4cbda41ef9baee1a470bd0fdb
LynyrdRoss <lyrhen17@gmail.com>
2016-10-05 13:40:08 +0800

    Updated BinarySearch

--- BSIS/Alquiroz-Lynyrd-Ross/binary-search/BinarySearch.java
+++ BSIS/Alquiroz-Lynyrd-Ross/binary-search/BinarySearch.java
@@ -1,3 +1,104 @@
+/*
+* @author: Lynyrd Ross Alquiroz
+* @link: https://github.com/LynyrdRoss/binary-search
+* @version: v1.2
+* @since: 10/03/2016
+*/
+
+import java.util.*;
+
+public class BinarySearch {
+
+	int [] arr;
+	int index, size, capacity;
+	int search = 0;
+
+	public BinarySearch (int size) {
+		capacity = size;
+		arr = new int [capacity];
+	}
+
+	public void sortArr (int [] num) {
+
+		int temp;
+
+		for (int x = 0; x < num.length - 1; x++) {
+			for (int y = 0; y < num.length - 1; y++) {
+				if (num[y] > num[y+1]) {
+					temp = num[y];
+					num[y] = num[y+1];
+					num[y+1] = temp;
+				}
+			}
+		}
+	}
+
+	public int binarySearch (int [] num, int find) {
+		int left, middle, right;
+
+		left = 0;
+		right = num.length - 1;
+		
+		while (right >= left) {
+			middle = (left + right) / 2;
+			if (num[middle] == find) {
+				return 1;
+			}
+			if (num[middle] < find) {
+				left = middle + 1;
+			}
+			if (num[middle] > find) {
+				right = middle - 1;	
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String [] args) {
+
+		Scanner in = new Scanner(System.in);
+		Random rand = new Random();
+
+		int userSize, numFind, target;
+
+		System.out.println();
+		System.out.print("Enter desired size of array: ");
+		userSize = in.nextInt();
+		System.out.println();
+
+		BinarySearch objClass = new BinarySearch(userSize);
+
+		System.out.print("Enter number to find: ");
+		numFind = in.nextInt();
+		System.out.println();
+
+		System.out.println("Array values: ");
+		for (int i = 0; i < userSize; i++) {
+			objClass.arr[i] = 1+(rand.nextInt(100));
+		}
+
+		objClass.sortArr(objClass.arr);
+
+		for (int i = 0; i < userSize; i++) {
+			System.out.print("[" + objClass.arr[i] + "] ");	
+		}
+		System.out.println();
+
+		target = objClass.binarySearch(objClass.arr, numFind);
+
+		for (int count = 0; count < userSize; count++) {
+			if (objClass.arr[count] == numFind) {
+				System.out.println(numFind + " is located at element: " + (count+1) + ".");
+			}
+		}
+
+		if (target == -1) {
+			System.out.println("Number is not present in the array.");
+		}
+	}
+}
+
 /**
 * BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
 * Each step, the size of array is reduced to half until one single element remain and a match is found.
@@ -7,7 +108,7 @@
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
 */
-
+/*
 import java.util.*; // note: imports all classes inside java.util
 
 public class BinarySearch {
@@ -71,3 +172,4 @@ public class BinarySearch {
 	}
 
 }
+*/
