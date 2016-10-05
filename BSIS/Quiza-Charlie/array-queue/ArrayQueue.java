a1b3bdd747f904d9e78369fbc4ef5fbb06f46cd4
Kizaken <lie.ruina@gmail.com>
2016-10-05 00:29:30 -0700

    Update ArrayQueue.java

--- BSIS/Quiza-Charlie/array-queue/ArrayQueue.java
+++ BSIS/Quiza-Charlie/array-queue/ArrayQueue.java
@@ -1,123 +1,91 @@
-/**
-* Java implementation of a queue using array.
-* For demo purpose, should hold a maximum of 10 integers.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
+public class Que {
+	int QSIZE;
+	private int[]queue;
+	private int nItems;
 
-public class ArrayStack {
-
-	 private int capacity;
-	  private int top = 0;
-	  private String[] storage;
-	  
-	  // constructor = initialize an object instance of the class
-	  public ArrayStack(int size) {
-	    capacity = size;
-	    storage = new String[capacity];
-	  }
-
-	  public void show() {
-	    // the top of the stack starts from array[0] then newer item gets added on top of another until it reaches its capacity limit
-	    for (int i = capacity-1; i >= 0; i--) {
-	      System.out.println("Stack ["+i+"] = "+storage[i]);
-	    }
-	    System.out.println();
-	  }
-	  
-	  private boolean isEmpty() {
-	    if (storage[0] == null) {
-	      // print error message
-	      System.out.println("The storage is empty");
-	      return true;
-	    } 
-	    return false;
-	  }
-
-	  private boolean isFull() {
-	    if (top == capacity) {
-	      // print error message
-	      System.out.println("Storage is Full");
-	      return true;
-	    } 
-	    return false;
-	  }
-
-	  public void push(String value) {
-	    if (isFull()) {
-	      // print error message
-	      System.out.println("Sorry!, the stack is Full");
-	    } else {
-	      System.out.println("... trying to push on stack[" + top + "] ...");
-	      storage[top] = value;
-	      /* increment or decrement top*/;
-	      top ++;
-	      System.out.println(value + " was successfully added.");
-	      System.out.println();
-	    }
-	  }
-
-	  public void pop() {
-	    if (isEmpty()) {
-	      // print error message
-	      System.out.println("Remove is Failed");
-	    } else {
-	      System.out.println("... trying to pop stack[" + (top-1) + "] ...");
-	      storage[top] = null;
-	      top --;
-	      System.out.println(storage[top] + " was successfully removed.");
-	      System.out.println();
-	    }
-	  }
-
-	  public void peek() {
-	    if (storage[top] == storage[0]) {
-	      System.out.println("PEEK TOP = " + storage[top]);
-	      System.out.println();
-	    } else {
-	      System.out.println("PEEK TOP = " + storage[top-1]);
-	      System.out.println();
-	    }
-	  }
-	  
-	  public static void main(String[] args) {
-	    // construct a new array queue and assign 10 as the integer value for the size limit of the stack
-		ArrayStack storage = new ArrayStack(10);
-	    System.out.println("STORAGE CAPACITY = " + storage.capacity );
-	    System.out.println();
-	    storage.show();// show empty stack
-	    storage.pop();// try removing on an empty stack
-	    storage.peek();// peek top element of an empty stack
-	    storage.push("one");
-	    storage.show();// show the updated stack
-	    storage.peek();// peek if top element is "one"
-	    storage.push("two");
-	    storage.show();// show the updated stack
-	    storage.peek();// peek if top element is "two"
-	    storage.push("three");
-	    storage.show();// show the updated stack
-	    storage.peek();// peek if top element is "three"
-	    storage.push("four");
-	    storage.show();// show the updated stack
-	    storage.push("five"); 
-	    storage.pop();// try removing "five"
-	    storage.push("six");
-	   
-	    storage.push("seven"); 
-	   
-	    storage.push("eight"); 
-	    
-	    storage.push("nine");
-	    
-	    storage.push("ten"); 
-	    storage.show();// show the updated stack
-	    storage.peek();// peek if top element is "ten"
-	    storage.push("eleven");// try adding "eleven"
-	    storage.push("twelve");// try adding "twelve"
-	    storage.show();// show the updated stack
-	  }  
-
+	public Que(int size){
+		this.QSIZE= size;
+		this.queue = new int[this.QSIZE];
 	}
+	public void showQueue(){
+		for (int i=0; i< this.QSIZE;i++){
+		System.out.println("QUEUE["+i+"] = " + this.queue[i]);
+		
+	}
+		System.out.println();
+}
+
+
+
+	public boolean isFull() {
+			if (this.nItems == this.QSIZE) {
+				System.out.println ("The Queue is already full");
+				return true;
+			} else{
+				return false;
+			}}
+			
+	public boolean isEmpty() {
+			if (this.nItems == 0){
+				System.out.println ("The Queue is Empty");
+				return true;
+			}	else{
+				return false;
+			}}
+
+	public boolean enqueue (int num) {
+			System.out.println("Trying to enqueue " + num + "...");
+			if (!this.isFull()){
+				this.queue[nItems] = num;
+				System.out.println("\t" + "Added " + num);
+				this.nItems++;
+					return true;
+			}	else{
+				return false;
+			}}
+
+	public void dequeue() {
+		 if (isEmpty()) {
+		    	System.out.println("REMOVE FAILED, Storage is empty.");
+		      System.out.println();
+		    } else {
+		      System.out.println("... trying to dequeue [" + (nItems) + "] ...");
+		      queue[nItems] = 0;
+		      nItems--;
+		      System.out.println(queue[nItems] + " was successfully removed.");
+		      System.out.println();
+		    }
+		 }
+	
+	public int peekFront() {
+	return this.queue[0];
+	}
+	public int peekRear() {
+			return this.queue[this.nItems-1];
+	}
+
+	public static void main(String[]args){
+		
+		Que queueDemo = new Que(10);
+				
+		
+		
+		queueDemo.enqueue(1);
+		queueDemo.enqueue(2);
+		queueDemo.dequeue();
+		queueDemo.enqueue(3);
+		queueDemo.enqueue(4);
+		queueDemo.enqueue(5);
+		queueDemo.enqueue(6);
+		queueDemo.enqueue(7);
+		queueDemo.enqueue(8);
+		queueDemo.enqueue(9);
+		queueDemo.enqueue(10);
+		queueDemo.showQueue();
+		queueDemo.enqueue(11);
+		queueDemo.showQueue();
+		queueDemo.enqueue(12);
+		
+		
+	}
+}
