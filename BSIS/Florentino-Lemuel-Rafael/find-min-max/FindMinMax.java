8911f310fbf5ade86df84241cc617b59d8138b20
Lemuel Rafael Florentino <lemuel.florentino19@gmail.com>
2016-10-05 18:19:20 +0800

    Update FindMinMax.java

--- BSIS/Florentino-Lemuel-Rafael/find-min-max/FindMinMax.java
+++ BSIS/Florentino-Lemuel-Rafael/find-min-max/FindMinMax.java
@@ -1,15 +1,44 @@
+import java.util.*;
 public class FindMinMax {
+	int [] arrange;
+	int capacity;
+	int size;
+	
+	public FindMinMax (int size){
+	capacity = size;
+	arrange = new int [capacity];
+	}
+	
 	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
 		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
+		int min = Integer.MAX_VALUE;
+		int arraysize;
+		
+		System.out.println("Enter size of an array: ");
+		arraysize = input.nextInt();
+		System.out.println();
+		
+		FindMinMax find = new FindMinMax (arraysize);
+		for (int a = 0; a < arraysize; a++){
+			find.arrange[a] = 1+(rand.nextInt(100));
+			
+			if (min > find.arrange[a]) {
+				min = find.arrange[a];
+			}
+			if (max < find.arrange[a]){
+				max = find.arrange[a];
+			}
+			
+			System.out.println(find.arrange[a]);
 		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		System.out.println();
+		
+		System.out.println("The largest number is " + max);
+		System.out.println("The smallest number is " + min);
+		
+		
+		
 	}
 }
