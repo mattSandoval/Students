c5bc129b57055ca95a2123a79193549007cb24df
Lemuel Rafael Florentino <lemuel.florentino19@gmail.com>
2016-10-05 19:14:53 +0800

    Update LinearSearch.java

--- BSIS/Florentino-Lemuel-Rafael/linear-search/LinearSearch.java
+++ BSIS/Florentino-Lemuel-Rafael/linear-search/LinearSearch.java
@@ -1,43 +1,48 @@
 /**
 * LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
 *
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
+* @author  [@lemlemz22]
+* @link    [https://github.com/lemlemz22]
+* @version [v1.0]
+* @update  [October 5, 2016]
 */
 
 import java.util.Scanner;
 import java.util.Random;
 
 public class LinearSearch {
-
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
+	
+	public static void main (String[] args){
+		
+		int a, arraysize, search, array[];
+		
+		Scanner input = new Scanner (System.in);
 		Random rand = new Random();
-
+		
 		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
+		arraysize = input.nextInt();
+		array = new int[arraysize];
 
-		System.out.print("Enter number to find: ");
+		System.out.print("Generated Numbers: ");
+		for (a=0 ; a<arraysize ; a++){
+			array[a]=rand.nextInt(1000);
+			System.out.print(array[a] + " ");
+		}
+		System.out.println();
+		
+		System.out.print("\nEnter number to find: ");
 		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
+		
+		for (a=0 ; a<arraysize ; a++){
+			
+			if (array[a] == search){
+				System.out.println("FOUND: " + search + " is at array[" + a + "].");
 				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
+			}
+			else {
+				System.out.println("NOT FOUND: " + search + " is not in the array.");
+				break;
+			}
+		}		
 	}
-
 }
