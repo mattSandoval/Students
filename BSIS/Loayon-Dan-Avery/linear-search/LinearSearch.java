fd595f25ab0e773d99237d86a4ea533d1b629dd1
DanAvery19 <loayondan@gmail.com>
2016-10-05 17:30:07 +0800

    Update LinearSearch.java

--- BSIS/Loayon-Dan-Avery/linear-search/LinearSearch.java
+++ BSIS/Loayon-Dan-Avery/linear-search/LinearSearch.java
@@ -1,43 +1,65 @@
-/**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
-*/
-
 import java.util.Scanner;
 import java.util.Random;
 
 public class LinearSearch {
+    
+    int array[];
+    int arraySize, search, find = 0;
+    
+    public LinearSearch(int size){
+        arraySize = size;
+        array = new int[arraySize];
+    }
 
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
+    public void randomVal(){
+        Random rand = new Random();
+        for ( int x = 0; x < arraySize; x++ ){
+            array[x] = rand.nextInt(1000);
+        }
+    }
+    
+    public void show(){
+        int spaceLim = 0;
+        System.out.println("Values of your Random Array:");
+        for ( int b = 0; b < arraySize; b++ ){
+            System.out.print( "["+b+"]: " + array[b] + "\t");
+            spaceLim += 1;
+            if ( spaceLim == 5 ){
+                System.out.println();
+                spaceLim = 0;
+            }
+        }
+        System.out.println();
+    }
+    
+    public void search(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the # you want to search: ");
+        search = input.nextInt();
+    } 
+    public void Find(){
+        for ( find = 0; find < array.length; find++ ){
+         if( array[find] == search )
+                System.out.println("The element # " + search + " is in' the Index[" + find + "]. ");
+         if( find == array.length - 1 && array[find] != search )
+            System.out.println("The search element is not in the Array Random Values.");
+        }
+    }
+     
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        
+       int size;
+       System.out.print("Please enter size of the Array: ");
+       size = input.nextInt();
+       
+       LinearSearch array = new LinearSearch(size);
+       
+       array.randomVal();
+       array.show();
+       array.search();
+       array.Find();
+      
+    }
+    
 }
