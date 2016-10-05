8722d2307789acee3f4871b62331ea9a8b62b0ff
kimOrozco <kimpossible1204@gmail.com>
2016-10-05 15:54:58 +0800

    Update LinearSearch.java

--- BSIS/Orozco-Kimberly/linear-search/LinearSearch.java
+++ BSIS/Orozco-Kimberly/linear-search/LinearSearch.java
@@ -1,10 +1,8 @@
 /**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
+* @author: Kimberly Orozco
+* @link    https://github.com/kimOrozco
 * @version 1.1
-* @since   22/09/2016
+* @since : [10/05/16]
 */
 
 import java.util.Scanner;
