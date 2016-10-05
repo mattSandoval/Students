192fca2325546131710ce15ab3de23d2f9be3433
Chyrine Cortez <zhynli30@gmail.com>
2016-10-05 15:38:36 +0800

    Add files via upload

--- BSIS/Cortez-Chyrine/linear-search/LinearSearch.java
+++ BSIS/Cortez-Chyrine/linear-search/LinearSearch.java
@@ -1,43 +1,35 @@
-/**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
-*/
-
-import java.util.Scanner;
-import java.util.Random;
-
+package cortez;
+import java.util.*;
 public class LinearSearch {
 
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
+	public static void main(String[]args){
+		
+	int size,find;
+	int []arr;
 		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
+		Random rad = new Random();
+		System.out.println("size many Output: ");
+		size= input.nextInt();
+		
+		arr= new int[size];
+		
+		for (int i=0; i<=size-1;i++){
+			arr[i]=rad.nextInt(1000);
+			System.out.println(arr[i]);
 		}
+		System.out.println("Searching for: ");
+		find=input.nextInt();
+		for(int c=0;c<=size-1;c++){
+			if(find==arr[c]){
+				System.out.println("Index no: "+ c);
+			break;
+			}
+			if (c==size-1){
 
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
+				System.out.print("the Number you searched is not in the Given Random!!!");
 
+			}
+
+				}
 	}
-
 }
