d72529992dcbfe2ed892ab0dc35e3e0d8baabe9c
norlietasedurante <norlietasedurante@gmail.com>
2016-10-05 17:29:12 +0800

    Update FindMinMax.java

--- BSIS/Sedurante-Norlieta/find-min-max/FindMinMax.java
+++ BSIS/Sedurante-Norlieta/find-min-max/FindMinMax.java
@@ -1,6 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+* Find minimum and maximum number in array 
+*
+* SOURCE CODE: FindMinMax.java
+* AUTHOR NAME: Sedurante, Norlieta  I.
+* GITHUB URL: [https://github.com/lvcc-dsa/Students/BSIS/Sedurante-Norlieta/find-min-max/FindMinMax.java]
+* LAST UPDATE: [10/05/2016]
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*/
+
 public class FindMinMax {
 	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
+		int[] numbers = { 7,5,6,64,8,9,9 };
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < numbers.length; i++) {
