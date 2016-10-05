c3fdce0872df9fe315ad9fec8c6ca0cec9ea7a00
maruthberce20 <ma.ruthberce20@gmail.com>
2016-10-05 13:16:21 +0800

    Update SumAveMax.java

--- ACT/Berce-Ruth/sum-ave-max/SumAveMax.java
+++ ACT/Berce-Ruth/sum-ave-max/SumAveMax.java
@@ -3,10 +3,10 @@
 * - Generate random numbers accordingly.
 * - Get the sum, average, and maximum value
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Ma.Ruth N. Berce]
+* @link    [https://github.com/maruthberce20]
 * @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @since   [last updated 10/05/2016]
 */
 
 import java.util.Scanner;
