20eeaaa617f5c49b84b330161b91e2d44f87c57d
KDpogz <bernz19XD@gmail.com>
2016-10-05 15:35:29 +0800

    Update LinearSearch.java

--- BSIS/Dayrit-Kent/linear-search/LinearSearch.java
+++ BSIS/Dayrit-Kent/linear-search/LinearSearch.java
@@ -1,43 +1,56 @@
 /**
 * LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
 *
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
-*/
-
-import java.util.Scanner;
-import java.util.Random;
-
+* @author  Dayrit Kent
+* @link   https://github.com/lvcc-dsa/Students/edit/master/BSIS/Dayrit-Kent/linear-search/LinearSearch.java
+* @version 1
+* @since  10/6/16
+*/package Linear;
+import java.util.*;
 public class LinearSearch {
 
-	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	Random rad = new Random();
 
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
+	int how,find;
+	int []array;
+	
+void numbers(){
+	System.out.println("How many Output: ");
+	how= input.nextInt();
+	
+	array= new int[how];
+	
+	for (int i=0; i<=how-1;i++){
+		array[i]=rad.nextInt(1000);
+		System.out.println(array[i]);
 	}
+	
+}
+void searching(){
 
+	System.out.println("Searching for: ");
+	find=input.nextInt();
+	for(int j=0;j<=how-1;j++){
+		if(find==array[j]){
+			System.out.println("Index no: "+ j);
+		break;
+		}
+		if (j==how-1){
+
+			System.out.print("the Number you searched is not in the Given Random!!!");
+
+		}
+
+			}
+
+}
+	
+	
+public static void main(String[]args){
+	LinearSearch object = new LinearSearch();
+	
+	object.numbers();
+	object.searching();
+}
 }
