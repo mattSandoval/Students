f37b7cb770b613266fab342b4cfb8cab5a4977a2
DanAvery19 <loayondan@gmail.com>
2016-10-05 17:15:45 +0800

    Update FindMinMax.java

--- BSIS/Loayon-Dan-Avery/find-min-max/FindMinMax.java
+++ BSIS/Loayon-Dan-Avery/find-min-max/FindMinMax.java
@@ -1,15 +1,20 @@
-public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
-	}
+public class FindMinMaxJava {
+    
+    public static void main(String[] args){
+        int[] numbers = { 23, 35, 123, 12412, 1345 };
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for ( int i = 0; i < numbers.length; i ++ ){
+            if (min > numbers[i])
+                min = numbers[i];
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+        System.out.println("Smallest number is "+ min );
+        System.out.println("Largest number is "+ max );
+
+             
+    }
+    
 }
+
