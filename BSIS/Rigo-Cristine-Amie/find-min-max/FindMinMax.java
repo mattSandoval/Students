0c3bf657bcab86b29beb06ca0c5b8a891eb19145
Amie Rigo (Do) <cristinerigo@gmail.com>
2016-10-05 15:26:06 +0800

    Update FindMinMax.java

--- BSIS/Rigo-Cristine-Amie/find-min-max/FindMinMax.java
+++ BSIS/Rigo-Cristine-Amie/find-min-max/FindMinMax.java
@@ -1,3 +1,9 @@
+/**
+* @author  [Cristine Amie Rigo]
+* @link    [https://github.com/amierigo/DSA/blob/master/BubbleSorting.java]
+* @version [1.0]
+* @since   [10-05-16]
+*/
 public class FindMinMax {
 	public static void main(String[] args) {
 		int[] numbers = { 88, 33, 55, 23, 64, 123 };
