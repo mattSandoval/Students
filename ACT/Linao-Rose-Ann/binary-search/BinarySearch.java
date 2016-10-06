2954b8622f6d9b68992e819d3c650f0fcb7a0b08
RoseAnnLinao <anndlin1031@gmail.com>
2016-10-06 11:27:56 +0800

    Update BinarySearch.java

--- ACT/Linao-Rose-Ann/binary-search/BinarySearch.java
+++ ACT/Linao-Rose-Ann/binary-search/BinarySearch.java
@@ -1,7 +1,4 @@
 /**
-* BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
-* Each step, the size of array is reduced to half until one single element remain and a match is found.
-*
 * @author  Rose Ann D. Linao
 * @link    https://github.com/RoseAnnLinao
 * @version 1.2
