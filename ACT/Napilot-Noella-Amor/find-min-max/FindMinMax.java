8ea7eab38252b25d826ef14b149cdbab5b8e3ed1
AmorNapilot <noellaamor.napilot@gmail.com>
2016-10-06 11:22:35 +0800

    Update FindMinMax.java

--- ACT/Napilot-Noella-Amor/find-min-max/FindMinMax.java
+++ ACT/Napilot-Noella-Amor/find-min-max/FindMinMax.java
@@ -1,15 +1,21 @@
-public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
-	}
+/**
+@author [Noella Amor Napilot]
+@ling [@AmorNapilot]
+@date-update [10-06-16]
+*/
+public class MinMax
+{
+    public static void main(String[] args) {
+        int [] numbers = {88, 33, 55, 23, 64, 123};
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length;i++){
+           if (min > numbers[i])
+           min = numbers[i];
+           if (numbers[i] > max)
+            max = numbers[i];
+        }
+        System.out.println("The smallest number is " + min);
+        System.out.println("The largest number is " + max);
+    }
 }
