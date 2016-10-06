30c018544b448c63c97792012d6dbe454ee868fb
samcoii22 <reyesnicko443@gmail.com>
2016-10-06 12:25:45 +0800

    Update BubbleSort.java

--- ACT/Reyes-Nicko/bubble-sort/BubbleSort.java
+++ ACT/Reyes-Nicko/bubble-sort/BubbleSort.java
@@ -1,41 +1,48 @@
 /**
-* BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
-* adjacent elements, then switching positions if they exist in the wrong order.
+* Java implementation of a stack using array.
+* For demo purpose, should hold a maximum of 10 string values.
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Nicko Fernandez Reyes]
+* @link    [https://github.com/samcoii22/]
 * @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @since   [10/6/2016]
 */
 
-// TODO: import Scanner and Random from Java library
-
-public class BubbleSort {
-
+mport java.util.Random;
+import java.util.*;
+public class BubbleSort
+{
+	
 	public static void main(String[] args) {
 
-		int array[], size, x, y, swap;
-
+		int array[], x, y, swap,size;
+       
 		Scanner input = new Scanner(System.in);
 		Random rand = new Random();
 
 		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
+		size = input.nextInt();// TODO: use nextInt() to assign keyboard input as array size
 		array = new int[size];
 
 		System.out.println();
 		System.out.println(size + " random numbers generated.");
 
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
+		for (x = 0; x < size; x++)
+		{
+			array[x] = (int)(Math.random()*1000 + 1);/// TODO: use nextInt() to assign random numbers (range of 1000) to array index
+			System.out.println(array[x]);// TODO: print unsorted numbers separated by space
 		}
+	
+		
 
 		System.out.println();
 		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
+		for (x = 0; x < size-1; x++)
+		{ // note: loop until before the end of array
+			for (y = 0; y < size-x-1; y++) 
+			{ // note: will not loop to numbers already sorted
+				if (array[y] > array[y+1]) 
+					{ // note: compare adjacent numbers which is greater
 					swap = array[y]; // note: swap positions...
 					array[y] = array[y+1]; // note: smaller will be move to the left
 					array[y+1] = swap; // note: larger will move to the right
@@ -46,8 +53,8 @@ public class BubbleSort {
 		System.out.println();
 
 		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
+		for (int c = 0; c <size;  c++) 
+			System.out.println(array[c]+" " );
+
 	}
 
-}
