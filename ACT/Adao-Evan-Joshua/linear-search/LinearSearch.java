899d3bdd4fe306e530b889d0185401455f7d6713
Evan Joshua A. Adao <joshuaenjoy24@gmail.com>
2016-10-07 03:02:40 +0800

    Update LinearSearch.java

--- ACT/Adao-Evan-Joshua/linear-search/LinearSearch.java
+++ ACT/Adao-Evan-Joshua/linear-search/LinearSearch.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+ */*[Adao Evan Joshua A.]
+    [@Joshua-Adao]
+    [2.0]
+    [10/06/2016]*/
+      
 package linearsearch;
 import java.util.*;
 import java.util.Random;
