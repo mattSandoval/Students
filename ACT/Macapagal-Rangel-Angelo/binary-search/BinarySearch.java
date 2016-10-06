bd4a99e86a6566077c2137ed5ee3f29845adcfd0
Rangel-Cruz-Macapagal <angelomacapagal52@gmail.com>
2016-10-06 12:03:10 +0800

    Update BinarySearch.java

--- ACT/Macapagal-Rangel-Angelo/binary-search/BinarySearch.java
+++ ACT/Macapagal-Rangel-Angelo/binary-search/BinarySearch.java
@@ -1,73 +1,50 @@
-/**
-* BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
-* Each step, the size of array is reduced to half until one single element remain and a match is found.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
-
-import java.util.*; // note: imports all classes inside java.util
-
-public class BinarySearch {
-
-	static int array[], index, size, search;
-
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-
-		System.out.println(size + " random numbers generated.");
+package binarysearch;
+import java.util.*;
+	public class binarysearch {
 		
-		for (int i = 0; i < array.length; i++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
+		public static void main(String[]args){
+		Scanner in= new Scanner(System.in);
+		Random t= new Random ();
+		
+		int[] array = new int[10];
+		int search =0,index;
+		int size;
+		
+		System.out.println("Enter Array Size: ");
+		size=in.nextInt();
+		array =new int[size];
+		
+		for(int i=0;i<size;i++){
+			array[i] = t.nextInt(1000);
+			System.out.println(array[i]+" ");
 		}
-		
-		// TODO: call bubbleSort method to sort the generated random numbers
-		System.out.println();
-		
-		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
-		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
-		
-		if (index != -1) {
-			System.out.println("FOUND: " + search + " is at " + "array["+index+"]");
+		System.out.print("Enter Number To be Find:");
+		search=in.nextInt();
+		index = binarysearch(array,search);
+		if(index!= -1){
+			System.out.println("Found at index " +index);
 		}
-		else {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
+		else{
+			System.out.println("Not Found!!");
 		}
-
-	}
-
-	static int binarySearch(int[] numbers, int find) { // note: @param array[], search
-
-		int left, right, middle;
-		// TODO: initialize value for left and right
-		
-		while (left <= right) {
-			middle = left + (right - left) / 2;
-			if (numbers[middle] == find) {
-				return middle;
-			} else if (find < numbers[middle]) {
-				right = middle - 1;
-			} else { // if numbers[middle] < find
-				left = middle + 1;
-			}
-		}
-		return -1; // note: exit loop if not found
-
-	}
+}
 	
-	static int bubbleSort(int[] arr) {
-
-		// TODO: sort the random array first before searching
-
+static int binarysearch(int[]numbers,int find){
+	int left, right, middle;
+	left=0;
+	right= numbers.length -1;
+	while(left <=right){
+		middle =(left +right)/2;
+		if(numbers[middle]==find){
+			return middle;
+		}
+		else if(numbers[middle] <find){ 
+			left=middle+1;
+		}
+		else{
+			right=middle-1;
+		}
 	}
-
+	return-1;
+}
 }
