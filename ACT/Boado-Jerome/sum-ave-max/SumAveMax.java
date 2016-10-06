574b002084937ed2e5b9b07779e7d91cca3cfe01
Jerome Boado <boadojerome@gmail.com>
2016-10-06 13:12:51 +0800

    Update SumAveMax.java

--- ACT/Boado-Jerome/sum-ave-max/SumAveMax.java
+++ ACT/Boado-Jerome/sum-ave-max/SumAveMax.java
@@ -3,58 +3,46 @@
 * - Generate random numbers accordingly.
 * - Get the sum, average, and maximum value
 *
-* @author  [your full name here]
+* @author  [Jerome L. Boado]
 * @link    [full github url]
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
 */
-
+package arraybasics;
 import java.util.Scanner;
 import java.util.Random;
+public class Arraybasics {
 
-public class SumAveMax {
-
-	private int sum, max, size, array[];
-	private float average;
-
-	public SumAveMax() {
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-		for (int i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			System.out.print(array[i] + " ");
-			sum += array[i];
-			if (array[i] > max)
-				max = array[i];			
-		}
-		System.out.println();
-	}
-
-	public void getSum() {
-		System.out.println("SUM = " + sum);
-	}
-
-	public void getMax() {
-		System.out.println("MAX = " + max);
-	}
-
-	public void getAverage() {
-		// note: type casting = assigning a value of one type to a variable of another type
-		average = (float) sum / size; // note: type cast int to float
-		System.out.println("AVERAGE = " + average);
-	}
-
-	public static void main(String[] args) {
-		SumAveMax demo = new SumAveMax();
-		System.out.println();
-		demo.getSum();
-		demo.getMax();
-		demo.getAverage();
-	}
-
+public static void main(String[] args) {
+Scanner Jerome = new Scanner(System.in);
+Random jerome = new Random();
+System.out.print("Enter Array Size? ");
+    int Jer = Jerome.nextInt();
+    int Laman[]= new int[Jer];
+    for (int j = 0; j < Laman.length; j++) {
+        Laman[j] = jerome.nextInt(100)+1;
+        System.out.println(" Random number for?  ["+(j+1)+"]: "+Laman[j]);
+     
+        
+    }
+       int total = 0;
+        for (int i = 0; i < Laman.length; i++) {
+        total = total+Laman[i];
+    }
+        System.out.println("SUM = "+ total);
+        double average = total/Laman.length;
+        System.out.println("Avarage = "+ average);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < Laman.length; i++) {
+               if ( Laman[i] > max)
+                max = Laman[i];
+        }
+   
+        System.out.println("MAX = "+max);
+        
+                    
+        
+    
+}
 }
