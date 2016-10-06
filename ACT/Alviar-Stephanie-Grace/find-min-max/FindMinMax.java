0f4619d41ffdc029df9dcab25cddfc2940d12860
Stephanie101 <stephaniealviar101@gmail.com>
2016-10-06 12:44:49 +0800

    Update FindMinMax.java

--- ACT/Alviar-Stephanie-Grace/find-min-max/FindMinMax.java
+++ ACT/Alviar-Stephanie-Grace/find-min-max/FindMinMax.java
@@ -1,3 +1,10 @@
+/**
+* @author  [Stephanie Alviar]
+* @link    [https://github.com/Stephanie101]
+* @version [version number]
+* @since   [last updated 6/10/2016]
+*/
+
 public class FindMinMax {
 	public static void main(String[] args) {
 		int[] numbers = { 88, 33, 55, 23, 64, 123 };
