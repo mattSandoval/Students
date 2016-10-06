e593d75351f7d54380f6a97d9352b258af311898
Jerome Boado <boadojerome@gmail.com>
2016-10-06 13:07:14 +0800

    Update FindMinMax.java

--- ACT/Boado-Jerome/find-min-max/FindMinMax.java
+++ ACT/Boado-Jerome/find-min-max/FindMinMax.java
@@ -1,15 +1,28 @@
+package findminmax;
+import java.util.Random;
+import java.util.Scanner;
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
-	}
-}
+
+    public static void main(String[] args) {
+        int[] numbers = {34,23,12,45,67,324};
+        Scanner Jerome = new Scanner(System.in);
+        Random jerome = new Random();
+        System.out.print("Enter Array Size? ");
+        int Jer = Jerome.nextInt();
+        int Laman[]= new int[Jer];
+    for (int j = 0; j < Laman.length; j++) {
+        Laman[j] = jerome.nextInt(100)+1;
+        System.out.println("Random Number: "+Laman[j]);
+        }
+    int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < Laman.length; i++) {
+            if ( min > Laman[i])
+                min = Laman[i];
+            if ( Laman[i] > max)
+                max = numbers[i];
+      
+    }
+      System.out.println("Smallest number is "+min);
+        System.out.println("LArgest number is "+max);
+    }}
