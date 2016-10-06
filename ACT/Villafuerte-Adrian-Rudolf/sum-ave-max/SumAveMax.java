5db0675ced81428493043e8ee3118bbaa0a776bd
UpeTheGreat2024 <adrian2022.al@gmail.com>
2016-10-06 14:04:21 +0800

    Update SumAveMax.java

--- ACT/Villafuerte-Adrian-Rudolf/sum-ave-max/SumAveMax.java
+++ ACT/Villafuerte-Adrian-Rudolf/sum-ave-max/SumAveMax.java
@@ -1,60 +1,42 @@
-/** 
-* - Accepts keyboard input to set array size.
-* - Generate random numbers accordingly.
-* - Get the sum, average, and maximum value
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
+
+/** @author  [Adrian Villafuerte]
+*  @since   [last updated 06/10/2016]
 */
 
 import java.util.Scanner;
 import java.util.Random;
-
 public class SumAveMax {
 
-	private int sum, max, size, array[];
-	private float average;
-
-	public SumAveMax() {
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-		for (int i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			System.out.print(array[i] + " ");
-			sum += array[i];
-			if (array[i] > max)
-				max = array[i];			
-		}
-		System.out.println();
-	}
-
-	public void getSum() {
-		System.out.println("SUM = " + sum);
-	}
-
-	public void getMax() {
-		System.out.println("MAX = " + max);
-	}
-
-	public void getAverage() {
-		// note: type casting = assigning a value of one type to a variable of another type
-		average = (float) sum / size; // note: type cast int to float
-		System.out.println("AVERAGE = " + average);
-	}
-
-	public static void main(String[] args) {
-		SumAveMax demo = new SumAveMax();
-		System.out.println();
-		demo.getSum();
-		demo.getMax();
-		demo.getAverage();
-	}
-
+public static void main(String[] args) {
+	Scanner upe = new Scanner(System.in);
+	Random Upe = new Random();
+		System.out.print("Please enter array size: ");
+  int size = upe.nextInt();
+  int capacity[]= new int[size];
+    		for (int a = 0; a < capacity.length; a++) {
+       	 capacity[a] = Upe.nextInt(100)+1;
+        	System.out.println(" Random number for?  ["+(a+1)+"]: "+capacity[a]);
+     
+        
+    }
+       		int total = 0;
+        	for (int i = 0; i < capacity.length; i++) {
+        	total = total+capacity[i];
+    }
+    	    System.out.println("SUM = "+ total);
+        	double average = total/capacity.length;
+        	System.out.println("Avarage = "+ average);
+        	int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < capacity.length; i++) {
+               if ( capacity[i] > max)
+                max = capacity[i];
+        }
+   
+        System.out.println("MAX = "+max);
+        
+                    
+        
+    
+}
 }
