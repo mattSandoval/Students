1eeb93898992f7e4deec1c89c31ca7c540cee684
Leo <lvibar228@gmail.com>
2016-10-06 13:06:28 +0800

    Update BubbleSort.java

--- ACT/Vibar-Leo/bubble-sort/BubbleSort.java
+++ ACT/Vibar-Leo/bubble-sort/BubbleSort.java
@@ -2,14 +2,15 @@
 * BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
 * adjacent elements, then switching positions if they exist in the wrong order.
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Leo O. Vibar]
+* @link    [https://github.com/Leovibar]
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
 */
 
-// TODO: import Scanner and Random from Java library
 
+import java.util.Scanner;
+import java.util.Random;
 public class BubbleSort {
 
 	public static void main(String[] args) {
@@ -20,34 +21,37 @@ public class BubbleSort {
 		Random rand = new Random();
 
 		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
+		size=input.nextInt();
 		array = new int[size];
 
 		System.out.println();
 		System.out.println(size + " random numbers generated.");
 
 		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
+	        array[x] = rand.nextInt(size);
+                System.out.println(array[x]);
+
 		}
 
 		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
+	
+		for (x = 0; x < size-1; x++) { 
+			for (y = 0; y < size-x-1; y++) { 
+				if (array[y] > array[y+1]) { 
+					swap = array[y]; 
+					array[y] = array[y+1]; 
+					array[y+1] = swap; 
 				}
 			}
 		}
 
-		System.out.println();
-
+		System.out.println("Sorted Numbers");
+                 for(int i=0;i<size;i++){
+		System.out.println(array[i]);
+                 }
 		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
+
+    }
 	}
 
 }
