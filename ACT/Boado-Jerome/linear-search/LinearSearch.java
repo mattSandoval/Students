9de8b589b3c3e69cda5b20e717870160dde8271f
Jerome Boado <boadojerome@gmail.com>
2016-10-06 13:09:46 +0800

    Update LinearSearch.java

--- ACT/Boado-Jerome/linear-search/LinearSearch.java
+++ ACT/Boado-Jerome/linear-search/LinearSearch.java
@@ -1,43 +1,36 @@
 /**
 * LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
 *
-* @author  Renaldo Valente
+* @author  Jerome L. Boado
 * @link    https://github.com/arrowbrave
 * @version 1.1
 * @since   22/09/2016
 */
 
+package lineararray;
 import java.util.Scanner;
 import java.util.Random;
+public class LinearArray {
 
-public class LinearSearch {
-
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
+    public static void main(String[] args) {
+    Scanner Jerome = new Scanner(System.in);
+    Random jerome = new Random();
+    int Laman[];
+    System.out.print("Enter Array Size? ");
+        int Size = Jerome.nextInt();
+         Laman= new int[Size];
+        System.out.print("Find number: ");   
+        int find = Jerome.nextInt();
+    for (int j = 0; j < Laman.length; j++) {
+        Laman[j] = jerome.nextInt(100)+1;
+        if (Laman[j]==find){
+            System.out.println("Found "+find+" at array ["+j+"]");
+        }
+        if (j==Laman.length-1){
+            System.out.println("Not Found "+find);
+        }
+       
+        }
+    }
+    
 }
