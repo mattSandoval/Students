78b8cd06d842096c217e3e162fe2a5e8980ca825
samcoii22 <reyesnicko443@gmail.com>
2016-10-06 12:06:22 +0800

    Update LinearSearch.java

--- ACT/Reyes-Nicko/linear-search/LinearSearch.java
+++ ACT/Reyes-Nicko/linear-search/LinearSearch.java
@@ -10,11 +10,8 @@
 
 import java.util.Scanner;
 import java.util.Random;
-public class Linear_Search {
+public class LinearSearch {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         int i, size, search, array[];
