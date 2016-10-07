f9d8d4b3564607ea125b7c842f2af222841dd4c7
norlietasedurante <norlietasedurante@gmail.com>
2016-10-07 10:19:32 +0800

    Update SumAveMax.java

--- BSIS/Sedurante-Norlieta/sum-ave-max/SumAveMax.java
+++ BSIS/Sedurante-Norlieta/sum-ave-max/SumAveMax.java
@@ -5,100 +5,85 @@
 
 * SOURCE CODE: SumAveMax.java
 * AUTHOR NAME: Sedurante, Norlieta I.
-* GITHUB URL: [https://github.com/norlietasedurante/Totalmax_Array]
-* LAST UPDATE: [09/01/2016]
+* GITHUB URL: [https://github.com/norlietasedurante/SumAveMax]
+* LAST UPDATE: [10/07/2016]
 *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 */
-package SumAveMax;
+
+package sumavemax;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class SumAveMax {
 
-    
+	static int[] array;
+	static int ArraySize;
+	static int sum;
 
-    
-   int Array[];
-    int sum;
-    int max;
-    int capacity; 
-    int arraysize;
-   
-    
-
-   public void show (){
-       Scanner input = new Scanner (System.in);
-       Random  nory = new Random();
-       System.out.print("enter number:");
-       arraysize = input.nextInt();
-       Array = new int [arraysize];
-        
-       for (int i = 1; i < arraysize; i++) {
-         
-         if (i%10==0){
-             System.out.println("\n");
-               
-         } 
-         else{
-             Array[i] = nory.nextInt(1000);
-             System.out.print("\t" + Array[i]);
-             
-         }
-          
-    
-    }
-    System.out.println();
-  }
-   
-   public void getsum(){
-       for ( int i =0; i<arraysize; i++)
-       {
-           sum +=Array[i];
-       }
-       System.out.println("Sum " + sum);
-       
-   }
-   public void getAve (){
-        for (int  b=0; b < arraysize; b++){
-            double average = sum /Array.length;
-        }
-        System.out.println("Ave " + sum/Array.length);
-   }
-   
-   public void getmax(){
-       for (int i =0; i< arraysize; i++)
-       {
-           if (Array[i]> max ) 
-                max = Array[i];
-       }
-       System.out.println("Max " + max);
-   }
-   
-   
     public static void main(String[] args) {
+
   
-    
-      Totalmax_Array A= new Totalmax_Array();
-      
-      
-    
-    System.out.println();
-    
-    A.show();
-     System.out.println();
-     
-    A.getsum();
-    A.getmax();
-    A.getAve();
-   
-  
-    
-    }
-
-
-    
-}
-
  
+ 
+            
+            Scanner Input = new Scanner(System.in);
+		Random Rand = new Random();
+		System.out.println("Enter number you want : "); 
+		ArraySize = Input.nextInt();
+		array = new int[ArraySize]; 
+                System.out.println();
+                for(int a=0; a<ArraySize;a++){
+                    array[a] = Rand.nextInt(1000);
+                }
+		input();
+		getSum();
+		getAve();
+		getMax();	
+	}
+	
+	static public void input(){	
+                int x=0;
+                        
+		for(int a = 0; a < ArraySize; a++){
+			System.out.print(array[a]+"\t");
+                        x+=1;
+                        if(x==10){
+                            System.out.println();
+                            x=0;
+                        }
+                        
+		}System.out.println();
+	}
+	
+	static public void getSum(){	
+		for(int a = 0; a < ArraySize; a++){
+			sum = sum + array[a];
+		}
+		
+		System.out.println("Sum = " + sum);
+	}
+	
+	static public void getAve(){
+		int ave = sum / ArraySize;
+		System.out.println("Average = " + ave);
+	}
+	
+	static public void getMax(){
+		int max = 0;
+		for(int a = 0; a < ArraySize; a++){
+			if(array[a] > max)
+				max = array[a];
+		}
+		System.out.println("Max = " + max);
+	}
+        
+	
+
+}
+    
+    
+
+    
+
