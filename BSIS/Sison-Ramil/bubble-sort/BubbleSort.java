ad3a3ba6d3dd7d8ada95eab38a91bcdd1620eedd
princeramil <princeramil.sison@gmail.com>
2016-10-08 10:36:51 +0800

    Update BubbleSort.java

--- BSIS/Sison-Ramil/bubble-sort/BubbleSort.java
+++ BSIS/Sison-Ramil/bubble-sort/BubbleSort.java
@@ -2,52 +2,57 @@
 * BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
 * adjacent elements, then switching positions if they exist in the wrong order.
 *
-* @author  [your full name here]
-* @link    [full github url]
+* @author  [Ramil Sison]
+* @link    []
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
 */
 
 // TODO: import Scanner and Random from Java library
 
+import java.util.*;
 public class BubbleSort {
 
-	public static void main(String[] args) {
-
-		int array[], size, x, y, swap;
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
+	public static void main (String[] args){
+		
+		int capacity, val, y, swap, arraysize[];
+		
+		Scanner ramil = new Scanner (System.in); 
+		Random sison = new Random ();
 
 		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
-		array = new int[size];
-
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
+		size = ramil.nextInt();
+	
+		array = new int [capacity];
+		for (int i = 0; i < arraysize.length; i++ ){
+		array [i] = sison.nextInt(1000);
+		
 		}
-
 		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
+		System.out.println(capacity + " random numbers generated ");
+		
+		for (val = 0; val < capacity; x++){
+			array [val] = sison.nextInt(1000);
+			System.out.print(arraysize[val] + " ");
+			
+		}
+		
+		System.out.println();
+		for (val=0; val < capacity -1; val++){
+			for (y = 0; y< capacity -val -1; y++){
+				
+			if (arraysize[y] > arraysize[y+1]){
+				swap = arraysize [y];
+			arraysize[y] = arraysize [y+1];
+			arraysize[y+1] = swap;
 				}
 			}
 		}
-
 		System.out.println();
-
-		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
+		System.out.println("Bubble Sorting Done! ");
+		for (val = 0; val < capacity ; val++){
+			System.out.print(arraysize [val] + " ");
+		}
 	}
-
+	
 }
