7aa4c82b5533c233ee7ce17c5e6442da76e0cf4a
Lemuel Rafael Florentino <lemuel.florentino19@gmail.com>
2016-10-10 22:30:51 +0800

    Update ArrayStack.java

--- BSIS/Florentino-Lemuel-Rafael/array-stack/ArrayStack.java
+++ BSIS/Florentino-Lemuel-Rafael/array-stack/ArrayStack.java
@@ -2,29 +2,27 @@
 * Java implementation of a stack using array.
 * For demo purpose, should hold a maximum of 10 string values.
 *
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
+* @author  Lemuel Rafael C. Florentino
+* @link    www.github.com/lemlemz22
+* @version v1.0
+* @update   October 10, 2016
 */
 
+import java.util.Arrays;
 public class ArrayStack {
 
-  // note: variable declarations
-  private int capacity; // note: max length of array
-  private int top = 0; // note: initialize a default value of top
-  private String[] storage; // note: array is of string values
+  private int capacity;
+  private int top = 0;
+  private String[] storage;
   
-  // note: constructor = initialize an object instance of the class
   public ArrayStack(int size) {
-    // todo: pass the value of size to capacity
-    storage = new String[/*__________*/]; // todo: update the array capacity
+    stackSize = size;
+    storage = new String[size];
   }
 
-  // note: will display the array
+  
   public void show() {
-    // note: the top of the stack starts from array[0] then newer item gets added on top of another until it reaches its capacity limit
-    for (int i = capacity-1; i >= 0; i--) {
+    for (int i = capacity; i >= 0; i--) {
       /* note: should print like this: 
       stack[9]   = 10th value
       stack[8]   = 9th value
@@ -34,95 +32,99 @@ public class ArrayStack {
       stack[0]   = 1st value
       */
     }
-    System.out.println(); // note: prints optional new line for readability
+    System.out.println("stack[" + i + "] = " + stackArray[i]);
   }
   
-  // note: will check if it's true that the array is empty
   private boolean isEmpty() {
-    if (storage[0] == null) { // note: compares if index[0] or top is null or empty
-      System.out.println(" "); // todo: print error message here
-      System.out.println(); // note: this prints optional new line for readability
+    if (storage[0] == null) {
+      System.out.println("STORAGE is EMPTY.");
+      System.out.println();
       return true;
     } 
-    return false; // note: defaults to false
+    return false;
   }
 
-  // note: will check if it's true that the array is full
   private boolean isFull() {
-    if (top == capacity) { // note: compares if top has reached capacity limit
-      System.out.println(" "); // todo: print error message here
+    if (top == capacity) {
+      System.out.println("STORAGE is FULL");
       return true;
     } 
-    return false; // note: defaults to false
+    return false;
   }
 
-  // note: will add value to the array
   public void push(String value) {
     System.out.println("... trying to push on stack[" + top + "] ...");
     if (isFull()) {
-      System.out.println(" "); // todo: print error message here
+      System.out.println("ADD FAILED.");
       System.out.println(); // note: prints optional new line for readability
     } else {      
-      storage[top]; // todo: assign the value to top
-      top; // todo: should increment or decrement?
-      System.out.println(value + " was successfully _________."); // todo: what does push do?
-      System.out.println(); // prints optional new line for readability
-    }
-  }
-
-  // note: will remove value to the array
-  public void pop() {
-    System.out.println("... trying to pop stack[" + (top-1) + "] ...");
-    if (isEmpty()) { // note: checks if empty, then stop
-      System.out.println(" "); // print error message here
-      System.out.println(); // prints optional new line for readability
-    } else {      
-      storage[top]; // todo: empty the value of top
-      top; // todo: should increment or decrement?
-      System.out.println(storage[top] + " was successfully _________."); // todo: what does pop do?
+      storage[top] = 0;
+      top++;
+      System.out.println(value + " was successfully _________.");
       System.out.println();
     }
   }
 
-  // note: will show the topmost value of the array
+  public void pop() {
+    System.out.println("... trying to pop stack[" + (top-1) + "] ...");
+    if (isEmpty()) { 
+      System.out.println("POP FAILED.");
+      System.out.println(); 
+    } else {      
+      storage[top] = null; 
+      top--;
+      System.out.println(storage[top] + " was successfully removed.");
+      System.out.println();
+    }
+  }
+
   public void peek() {
     if (isEmpty()) { 
       System.out.println("PEEK TOP = NULL");
       System.out.println();
     } else {
-      System.out.println("PEEK TOP = " + storage[top]);
+      System.out.println("PEEK TOP = " + storage[top-1]);
       System.out.println();
     }
   }
   
   public static void main(String[] args) {
-    // construct a new array queue and assign 10 as the integer value for the size limit of the stack
-    System.out.println("STORAGE CAPACITY = " + /* return the size limit */);
+    storage theStack = new storage (10);
+    System.out.println("STORAGE CAPACITY = " + 10);
     System.out.println();
-    // show empty stack
-    // try removing on an empty stack
-    // peek top element of an empty stack
+			theStack.show();
+			theStack.pop();
+			theStack.peek();
     storage.push("one");
-    // show the updated stack
-    // peek if top element is "one"
+			theStack.show();
+			theStack.peek();
     storage.push("two");
-    // show the updated stack
-    // peek if top element is "two"
+			theStack.show();
+			theStack.peek();
     storage.push("three");
-    // show the updated stack
-    // peek if top element is "three"
+			theStack.show();
+			theStack.peek();
     storage.push("four");
-    // show the updated stack
+			theStack.show();
+			theStack.peek();
     storage.push("five"); 
-    // show the updated stack
-    // try removing "five"
-    storage.push("six"); 
+			theStack.show();
+			theStack.peek();
+    storage.push("six");
+      theStack.show();
+			theStack.peek();
     storage.push("seven"); 
-    storage.push("eight"); 
-    storage.push("nine");    
+			theStack.show();
+			theStack.peek();
+    storage.push("eight");
+    	theStack.show();
+			theStack.peek();
+    storage.push("nine");
+    	theStack.show();
+			theStack.peek();
     storage.push("ten"); 
-    // show the updated stack
-    // peek if top element is "ten"
+			theStack.show();
+			theStack.peek();
     // try adding "eleven"
     // try adding "twelve"
     // show the updated stack
