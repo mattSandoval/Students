2ba05a8f4a6c96c4ce80b6594749a1f0e0d64deb
reinerio de guzman <jeffersonechipare01@gmail.com>
2016-10-10 22:26:33 +0800

    Update LinearSearch.java

--- ACT/De-Guzman-Reinerio-Jr/linear-search/LinearSearch.java
+++ ACT/De-Guzman-Reinerio-Jr/linear-search/LinearSearch.java
@@ -1,43 +1,34 @@
-/**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
+/*
+Reinerio H. de Guzman
+ACT-2
 */
+package linearsearch;
 
 import java.util.Scanner;
 import java.util.Random;
 
 public class LinearSearch {
 
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
+        public static void main(String[] args) {
+    Scanner Reinerio = new Scanner(System.in);
+    Random rei = new Random();
+    int deg[];
+    System.out.print("Enter Array Size? ");
+        int Size = Reinerio.nextInt();
+         deg= new int[Size];
+        System.out.print("Find number: ");   
+        int find = Reinerio.nextInt();
+    for (int r = 0; r < deg.length; r++) {
+        deg[r] = rei.nextInt(100)+1;
+        if (deg[r]==find){
+            System.out.println("Found "+find+" at array ["+r+"]");
+        }
+        if (r==deg.length-1){
+            System.out.println("Not Found "+find);
+        }
+       
+        }
+    }
+    
 }
+
