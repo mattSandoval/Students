d778d60cfa0f47fece18c8d284a0466eca2ec522
⊂hristian ⊃iverson <diversonchristian@gmail.com>
2016-10-11 12:45:11 +0800

    Update BinarySearch.java

--- ACT/Diverson-Christian/binary-search/BinarySearch.java
+++ ACT/Diverson-Christian/binary-search/BinarySearch.java
@@ -12,7 +12,7 @@ import java.util.*; // note: imports all classes inside java.util
 
 public class BinarySearch {
 
-	static int array[], index, size, search;
+	static int array[], index, size, search, space;
 
 	public static void main(String[] args) {
 
@@ -27,13 +27,24 @@ public class BinarySearch {
 		
 		for (int i = 0; i < array.length; i++) {
 			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-		}
-		
+			array[i] = rand.nextInt(1000);
+		} /* ipapakita yung nagenerate na random numbers
+		for (int z = 0; z < array.length; z++) {
+                    System.out.print (array[z]+"\t");
+                    space+=1;
+                    if (space == 5){
+                        System.out.println();
+                        space=0;
+                    } 
+                } */
+                
 		// TODO: call bubbleSort method to sort the generated random numbers
+		bubbleSort(array);
 		System.out.println();
 		
 		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
+		// TODO: use nextInt() to assign keyboard input as search item
+		search = input.nextInt();	
 		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
 		
 		if (index != -1) {
@@ -49,6 +60,9 @@ public class BinarySearch {
 
 		int left, right, middle;
 		// TODO: initialize value for left and right
+		left = 0;
+		right = array.length-1;
+                
 		
 		while (left <= right) {
 			middle = left + (right - left) / 2;
@@ -65,9 +79,28 @@ public class BinarySearch {
 	}
 	
 	static int bubbleSort(int[] arr) {
-
+        int temp = 0;
 		// TODO: sort the random array first before searching
-
+		for (int a = 0; a < array.length; a++) {
+		   for (int b = 0; b < a; b++) {
+   		      if (array[a] < array[b]) {
+      		      temp = array[a];
+      		      array[a] = array[b];
+      		      array[b] = temp;
+                      }
+		   }
+                } /* ipiprint yung sinort na random numbers
+                System.out.println();
+                System.out.print("Sorted\n");
+                for (int z = 0; z < array.length; z++){
+                       System.out.print(array[z]+"\t");
+                       space+=1;
+                       if (space==5){
+                           System.out.println();
+                           space=0;
+                       }
+		} */
+            return 0;
 	}
-
 }
+
