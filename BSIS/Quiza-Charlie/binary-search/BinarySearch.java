93c3de7133e9ddee92d4069e94e807c28986c6a0
Kizaken <lie.ruina@gmail.com>
2016-10-12 22:13:43 +0800

    Add files via upload

--- BSIS/Quiza-Charlie/binary-search/BinarySearch.java
+++ BSIS/Quiza-Charlie/binary-search/BinarySearch.java
@@ -1,73 +1,102 @@
-/**
-* BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
-* Each step, the size of array is reduced to half until one single element remain and a match is found.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
-
-import java.util.*; // note: imports all classes inside java.util
+import java.util.Random;
+import java.util.Scanner;
 
 public class BinarySearch {
-
-	static int array[], index, size, search;
-
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-
-		System.out.println(size + " random numbers generated.");
-		
-		for (int i = 0; i < array.length; i++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-		}
-		
-		// TODO: call bubbleSort method to sort the generated random numbers
-		System.out.println();
-		
-		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
-		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
-		
-		if (index != -1) {
-			System.out.println("FOUND: " + search + " is at " + "array["+index+"]");
-		}
-		else {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
-	static int binarySearch(int[] numbers, int find) { // note: @param array[], search
-
-		int left, right, middle;
-		// TODO: initialize value for left and right
-		
-		while (left <= right) {
-			middle = left + (right - left) / 2;
-			if (numbers[middle] == find) {
-				return middle;
-			} else if (find < numbers[middle]) {
-				right = middle - 1;
-			} else { // if numbers[middle] < find
-				left = middle + 1;
-			}
-		}
-		return -1; // note: exit loop if not found
-
-	}
 	
-	static int bubbleSort(int[] arr) {
+	 static int size,array[],search,y,x,temp;
+	 
+	 
+	 public void sizeofArray(){    
+	    Scanner in = new Scanner(System.in);
+	    Random rand = new Random(); 
+	    System.out.println("Enter the size of the array: ");
+	    size = in.nextInt();
+	    array = new int[size];
+	     for(int i=0;i<array.length;i++){
+	      array[i] = rand.nextInt(1000);
+	    } 
+	  }
+	
+	 
+	  static int binarySearch(){
+		    int left,right,middle;
+		    left = 0;
+		    right = array.length - 1;
+		    
+		     Scanner in = new Scanner(System.in);
+		     System.out.println("\nEnter the number you are looking for: ");
+		     search = in.nextInt();
+		  
+		     while(left <=right){
+		      middle=(left+right) / 2;
+		      if (array[middle] ==search){
+		        return middle;
+		      }
+		      else if (array[middle]<search){
+		        left = middle + 1;
+		      }
+		      else{
+		        right = middle - 1;
+		      }
+		    }
+		    return -1;
+		  }
+	
+	  
+    	
+	
+	public void randomArray(){
+		
+		 System.out.println("\n" + size + " random numbers generated.");
+	     for ( y = 0; y <= size-1; y++){
+	       System.out.print(array[y] + " ");
 
-		// TODO: sort the random array first before searching
-
+	       for(x = 0; x < size-y-1; x++){
+	         if(array[x] > array[x+1]){
+	           temp = array[x];
+	           array[x] = array[x+1];
+	           array[x+1] = temp;
+	         }
+	       }
+	      }
+	      
+	      System.out.println("\n\nSorted Numbers:");
+	      for(y = 0; y < size; y++){
+	      System.out.print(array[y] + " ");
+	     }
+	      System.out.println("\n\nBubbleSorting Done!.");
 	}
+	public void show(){
+	    int i;
+	    
+	    for(i = 0; i < array.length; i++){
+	      if (search == array[i]){
+	        System.out.println("Found at array["+i+"].");
+	        break;
+	      }
+	    }
+	    if (i == array.length){
+	      System.out.println("Value not found.");
+	    }
+	  }
+	
 
+	
+
+	public static void main (String[] Args){
+
+		BinarySearch summon = new BinarySearch ();
+		
+		summon.sizeofArray();
+		summon.randomArray();
+		summon.binarySearch();
+		summon.show();
+		
+		
+	
+		
+	
+	}
 }
+
+
