9e3f056e0db693941f3b2ed2caa379e991a21642
janbertartuza23 <artuzajanbert23@gmail.com>
2016-10-13 07:17:32 -0700

    Update LinearSearch.java

--- ACT/Artuza-Janbert/linear-search/LinearSearch.java
+++ ACT/Artuza-Janbert/linear-search/LinearSearch.java
@@ -1,43 +1,43 @@
 /**
 * LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
 *
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
+* @author  Janbert Q Artuza
+* @link    https://github.com/janbertartuza23
+* @version 1.0
+* @since   13/Oct/2016
 */
 
 import java.util.Scanner;
 import java.util.Random;
 
 public class LinearSearch {
-
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
+ public static void main(String[] args){
+	 int a,size,search,array[];
+	 
+	 Scanner input = new Scanner(System.in);
+	 Random rand= new Random();
+	 
+	 System.out.print("enter array size: ");
+	 size = input.nextInt();
+	 array= new int[size];
+	 
+	 System.out.print("enter number to find: ");
+	 search=input.nextInt();
+	 
+	 for( a=0; a<size; a++) { 
+		 array[a]=rand.nextInt(1000);
+	 
+	 	if (array[a]==search) {
+	 		System.out.println("found: "+search+"is at "+"array["+a+"]");
+	 	
+	 	break;
+	 }	
+	 	}
+	 
+	 if(a==size){
+		 System.out.println("Not Found: "+search+" is not in the array.");
+	 }
+	 
+	 
+ }
 }
