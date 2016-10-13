4c14c68322229d0d57fd7a35cff871221cd15217
janbertartuza23 <artuzajanbert23@gmail.com>
2016-10-13 06:49:21 -0700

    Update and rename FindMinMax.java to findminmax.java

---
+++ ACT/Artuza-Janbert/find-min-max/findminmax.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class findminmax {
+ public static void main(String[]args) {
+
+	 Scanner input = new Scanner(System.in);
+
+	 int [] numbers = {20,45,68,98,32,12};
+	 int min = Integer.MAX_VALUE;
+	 int max = Integer.MIN_VALUE;
+	 
+	 System.out.println("this is the numbers: 20,45,68,98,32,12\n");
+	 
+	 for(int a=0; a<numbers.length; a++) {
+		 if(min>numbers[a]){
+			 min= numbers[a];
+		 }
+		   if(numbers[a]>max){
+			 max= numbers[a];
+		 }
+		
+	 }
+	 System.out.println("smallest value: "+min);
+	 System.out.println("largest value: "+max);
+	 
+ }
+}
