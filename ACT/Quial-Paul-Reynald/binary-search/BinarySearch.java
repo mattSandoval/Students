2e1e2aba783c0f39923685614501003aa377b44a
Paul Reynald Quial <paulrenaldquial@gmail.com>
2016-10-14 12:19:32 -0700

    Add files via upload

--- ACT/Quial-Paul-Reynald/binary-search/BinarySearch.java
+++ ACT/Quial-Paul-Reynald/binary-search/BinarySearch.java
@@ -1,73 +1,83 @@
-/**
-* BINARY SEARCH is a Divide and Conquer algorithm that compares the item with the middle element of a sorted (ascending) array. 
-* Each step, the size of array is reduced to half until one single element remain and a match is found.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
-
-import java.util.*; // note: imports all classes inside java.util
+import java.util.Scanner;
 
 public class BinarySearch {
-
-	static int array[], index, size, search;
-
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];
-
-		System.out.println(size + " random numbers generated.");
-		
-		for (int i = 0; i < array.length; i++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-		}
-		
-		// TODO: call bubbleSort method to sort the generated random numbers
-		System.out.println();
-		
-		System.out.print("Enter number to find: ");
-		// TODO: use nextInt() to assign keyboard input as search item	
-		index = binarySearch(array, search); // note: assign the search result to array index where it was found	
-		
-		if (index != -1) {
-			System.out.println("FOUND: " + search + " is at " + "array["+index+"]");
-		}
-		else {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
-	static int binarySearch(int[] numbers, int find) { // note: @param array[], search
-
-		int left, right, middle;
-		// TODO: initialize value for left and right
-		
-		while (left <= right) {
-			middle = left + (right - left) / 2;
-			if (numbers[middle] == find) {
-				return middle;
-			} else if (find < numbers[middle]) {
-				right = middle - 1;
-			} else { // if numbers[middle] < find
-				left = middle + 1;
-			}
-		}
-		return -1; // note: exit loop if not found
-
+	private static int capacity;
+    private int begin=0;
+    private static int[] numbers;
+    
+	public BinarySearch(int size){
+        capacity = size;
+        numbers = new int[capacity];
+    }
+	
+	public void push(int value){
+        System.out.print("'" + value + "' was succesfully added.\n\n");
+        numbers[begin] = value;
+        begin++;
+    }
+	
+	public int search(){
+		int left, right, middle, find;
+        Scanner input = new Scanner (System.in);
+        
+        left = 0;
+        right = capacity-1;
+    	middle = left + (right-left) / 2;
+    	
+        System.out.print("\n\nWhat is the number you want to find? ");
+        find = input.nextInt();
+        
+        while (left < right){
+        	if (BinarySearch.numbers[middle] == find){
+        		System.out.print("Found! The number is at index[" + middle + "]");
+        		return middle;
+        	}
+        	else if (find < BinarySearch.numbers[middle]){
+        		left = middle + 1;
+                System.out.print("Not found!");
+        	}
+        	else {
+        		right = middle - 1;
+                System.out.print("Not found!\n");
+        	}
+        }
+        
+        System.out.print("\nThe left is " + BinarySearch.numbers[left]);
+        System.out.print("\nThe middle is " + BinarySearch.numbers[middle]);
+        System.out.print("\nThe right is " + BinarySearch.numbers[right]);
+        return 0;
 	}
 	
-	static int bubbleSort(int[] arr) {
-
-		// TODO: sort the random array first before searching
-
-	}
-
+	public static void main(String [] args){
+		int limit=0;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("\t*-------------* Binary Search *-------------*\n\n");
+        System.out.print("What is the capacity of storage: ");
+        limit = input.nextInt();
+        BinarySearch numbers = new BinarySearch(limit);
+       
+        System.out.print("STORAGE CAPACITY: " + limit + "\n\n");
+        
+        for (int i=0 ; i<limit ; i++){
+        System.out.print("Input number[" + i + "]: ");
+        int user = input.nextInt();
+        numbers.push(user);
+        }
+        
+        
+        for (int x=0 ; x<limit ; x++){
+        	if (BinarySearch.numbers[x] < min)
+        		min = BinarySearch.numbers[x];
+        	if (BinarySearch.numbers[x] > max)
+        		max = BinarySearch.numbers[x];
+        }
+        
+        System.out.println("Smallest number is " + min);
+        System.out.println("Largest number is " + max);
+        
+        numbers.search();
+    }
 }
