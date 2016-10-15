ccad49f966f77d216e5a96eb1b2ed4c73b37d24a
dhansaludo <danmarksaludo@gmail.com>
2016-10-15 12:43:30 +0800

    Add files via upload

--- ACT/Saludo-Dan-Mhark/find-min-max/FindMinMax.java
+++ ACT/Saludo-Dan-Mhark/find-min-max/FindMinMax.java
@@ -1,15 +1,23 @@
+package hola;
+import java.util.Scanner;
+import java.util.Random;
+
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
+	public static void main(String[] args){
+		int[] numbers = {88,33,55,23,64,123};
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
+		for (int i = 0; i <numbers.length; i++) {
+			if (min > numbers[i])
 				min = numbers[i];
 			if (numbers[i] > max)
 				max = numbers[i];
+		
 		}
 		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		System.out.println("Largest number is " + max);
+		
+			
 	}
+
 }
