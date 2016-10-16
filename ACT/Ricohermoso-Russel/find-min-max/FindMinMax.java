5bf3bdd798a3ff02a93d0becf10b247d686f73f5
Russel Ricohermoso <ricohermosorussel@gmail.com>
2016-10-16 16:31:21 +0800

    Update FindMinMax.java

--- ACT/Ricohermoso-Russel/find-min-max/FindMinMax.java
+++ ACT/Ricohermoso-Russel/find-min-max/FindMinMax.java
@@ -1,15 +1,32 @@
+/*Russel Ricohermoso
+ACT-2
+ */
+package findminmax;
+import java.util.Random;
+import java.util.Scanner;
+
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
-	}
-}
+
+    public static void main(String[] args) {
+        int[] numbers = {34,23,12,45,67,324};
+        Scanner russel = new Scanner(System.in);
+        Random sel = new Random();
+        System.out.print("Enter Array Size? ");
+        int sel = russel.nextInt();
+        int wews[]= new int[rein];
+    for (int j = 0; j < wews.length; j++) {
+        wews[j] = sel.nextInt(100)+1;
+        System.out.println("Random Number: "+deg[j]);
+        }
+    int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < deg.length; i++) {
+            if ( min > wews[i])
+                min = wews[i];
+            if ( wews[i] > max)
+                max = numbers[i];
+      
+    }
+      System.out.println("Smallest number is "+min);
+        System.out.println("LArgest number is "+max);
+    }}
