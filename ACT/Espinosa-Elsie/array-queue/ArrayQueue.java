ed48e49661f7dedf1794b874ad9e4ce349b4f4c6
elsieespinosa10 <eelsie06@gmail.com>
2016-10-15 22:41:49 -0700

    Update ArrayQueue.java

--- ACT/Espinosa-Elsie/array-queue/ArrayQueue.java
+++ ACT/Espinosa-Elsie/array-queue/ArrayQueue.java
@@ -21,7 +21,7 @@ public class ArrayQueue {
     capacity = size; // note: optional, can directly pass a new int[size]
     storage = new int[capacity];
     head=0;
-    tail=0;
+    tail=-1;
     // todo: initialize values for tail, head, and tail
   } 
 
