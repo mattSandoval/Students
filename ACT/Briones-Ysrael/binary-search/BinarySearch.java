49658d09cc5519ffd7112582ee292102550501e0
Ysrael Briones <bible_affliction11@yahoo.com>
2016-10-17 09:40:45 +0800

    Update BinarySearch.java

--- ACT/Briones-Ysrael/binary-search/BinarySearch.java
+++ ACT/Briones-Ysrael/binary-search/BinarySearch.java
@@ -8,43 +8,47 @@
 * @since   [last updated 17/10/2016]
 */
 
-import java.util.*;
+import java.util.Scanner;
 public class BinarySearch {
-    public static void main(String[] args) {
-        int[] array = new int [5];
-        int search = 0, index;
-        
-        System.out.println("Enter 5 numbers: ");
-        Scanner input = new Scanner(System.in);
-        for (int i = 0; i < array.length; i++) {
-            array[i] = input.nextInt();
-        }
-        System.out.print("Enter number to find: ");
-        search = input.nextInt();
-        index = binarysearch(array, search);
-        if (index != -1) {
-            System.out.println("FOUND at index " + index);            
-        }else {
-            System.out.println("NOT FOUND");
-        }
-    }
-    
-    static int binarysearch(int[] numbers, int find) {
-        int left, right, middle;
-        left = 0;
-        right = numbers.length - 1;
-        while (left <= right) {
-            middle = (left + right) / 2;
-            if (numbers[middle] == find) {
-                return middle;
-            }else if (numbers[middle] < find){
-                left = middle + 1;
-            }else {
-                right = middle - 1;
-            }
-        }
-        return -1;
-        
-    }
-    
+
+  public static void main(String args[])
+  {
+    int c, first, last, middle, n, search, array[];
+ 
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter array size: ");
+    n = in.nextInt(); 
+    array = new int[n];
+ 
+    System.out.println("Enter " + n + " integers");
+ 
+ 
+    for (c = 0; c < n; c++)
+      array[c] = in.nextInt();
+ 
+    System.out.print("Enter number to find: ");
+    search = in.nextInt();
+ 
+    first  = 0;
+    last   = n - 1;
+    middle = (first + last)/2;
+ 
+    while( first <= last )
+    {
+      if ( array[middle] < search )
+        first = middle + 1;    
+      else if ( array[middle] == search ) 
+      {
+        System.out.println(search + " is found at index " + (middle + 1) + ".");
+        break;
+      }
+      else
+         last = middle - 1;
+ 
+      middle = (first + last)/2;
+   }
+   if ( first > last )
+      System.out.println(search + " is not in the array.\n");
+  }
 }
