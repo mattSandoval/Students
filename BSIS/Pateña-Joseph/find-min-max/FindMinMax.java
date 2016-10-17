ac85fd0478c25ae004b4ff3b601ffd04e21f003d
Joseph Pateña <jpatena16@gmail.com>
2016-10-17 16:34:31 +0800

    Update FindMinMax.java

--- BSIS/Pateña-Joseph/find-min-max/FindMinMax.java
+++ BSIS/Pateña-Joseph/find-min-max/FindMinMax.java
@@ -1,15 +1,64 @@
+import java.util.Scanner;
+import java.util.Random;
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
+	
+	private int capacity;
+	public int total=0;
+	public double average=0;
+	public int []array;
+	
+	public FindMinMax (int size) {
+		capacity = size;
+		array = new int [capacity];
+	}
+	
+	public void rand() {
+			
+		Random random = new Random();
+		for (int i = 0; i <array.length ; i++) {	
+			int x = random.nextInt(100);
+			array[i] = x;
 		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
+		
+		System.out.println(capacity+" RANDOM NUMBERS GENERATED.");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("array["+i+"] = "+array[i]);
+		}
+		System.out.println();
+		
+	}
+
+	public void FindMinMax() {
+		int largest = 0, smallest=array[0], min=0, max = 0;
+		for (int i = 0; i < array.length; i++) {
+			
+			if(array[i] <= smallest) {
+				min = array[i];
+				smallest = array[i];
+			}
+			if(array[i] > largest){
+				max = array[i];
+				largest = array[i];
+			}
+		}
+		
+		System.out.println("Largest number: "+max);
+		System.out.println("Smallest number: "+min);
+	}
+	
+	public static void main (String [] args) {
+		
+		Scanner input =  new Scanner(System.in);
+		
+		System.out.print("Enter the size of your array: ");
+		int size = input.nextInt();
+		System.out.println();
+		
+		FindMinMax implement = new FindMinMax(size);
+		
+		implement.rand();
+		implement.FindMinMax();
+
 	}
 }
+
